e3c16345178799f944e981f81d6c8b6894840139
Carla Ares <carla.ares@honou.com.ar>
2012-04-06 12:01:27 -0300

    devise configured. sing up working.

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, ,   and :omniauthable
+  devise :database_authenticatable, :registerable, :lockable, :timeoutable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :encryptable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+end
