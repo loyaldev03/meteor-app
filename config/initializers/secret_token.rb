e1ffc233c69842808f6ffa8bdbcaa63577f1e4a7
Carla Ares <carla.ares@honou.com.ar>
2012-04-06 10:42:30 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SacPlatform::Application.config.secret_token = 'bb94c459af3dd5e8b5c4e32e527f6fc8df82f169e41e4daddd8a21531d9a291742c3e4551e146cf1c1b57cf0d5af462418ac9e7aa8e917b15f902e7d39e7557e'
