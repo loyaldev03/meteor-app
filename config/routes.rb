7e57a2f1a875f156488a2754eb4e842c9c67776d
sebastian <sebastian@xagax.com>
2012-04-11 11:07:13 -0300

    #18246 - Commented match line

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ SacPlatform::Application.routes.draw do
 
   namespace :admin do
     resources :users do
-    get :lock
-    get :unlock
+      get :lock
+      get :unlock
     end
   end
 
@@ -65,5 +65,5 @@ SacPlatform::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-   match ':controller(/:action(/:id))(.:format)'
+  # match ':controller(/:action(/:id))(.:format)'
 end
