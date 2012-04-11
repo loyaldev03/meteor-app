1bea35775485641f0f3f1e206f1dc8077cf4d9e4
sebastian <sebastian@xagax.com>
2012-04-11 13:29:50 -0300

    #18260 - Testing modified

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,5 +1,17 @@
-Factory.define :user do |u|
-  u.password "secret"
-  u.password_confirmation {|u| u.password}
-  u.sequence(:email) {|n| "carla#{n}@test.no" }
+# Factory.define :user do |u|
+#   u.username "seba"
+#   u.password "secret"
+#   u.password_confirmation {|u| u.password}
+#   u.sequence(:email) {|n| "carla#{n}@test.no" }
+# end
+# Read about factories at https://github.com/thoughtbot/factory_girl
+FactoryGirl.define do
+
+
+  factory :user do
+    password "secret"
+    password_confirmation {password}
+    sequence(:email) {|n| "carla#{n}@test.no" }
+  end
+
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do 
+    assert true
+  end
+
+  test "Shouldnt be two users with samen name" do
+  	  user = FactoryGirl.build(:user)
+     user.username = "user"
+     user.save
+  	  assert !user.new_record?, "There are two users with the same name" 
+  end
+
+
+
+
 end
