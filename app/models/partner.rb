c9a9e42929e18a2ae109d1a3d2f226488dadd340
sebastian <sebastian@xagax.com>
2012-04-12 10:54:35 -0300

    #18333 - Fixed Partners's ABM interface

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,12 +1,10 @@
 class Partner < ActiveRecord::Base
-  attr_accessible :contract_uri, :deleted_at, :description, :name, :prefix, :website_url
+  attr_accessible :contract_uri, :deleted_at, :description, :name, :prefix, :website_url, :logo
+  
+  acts_as_paranoid
+  validates_presence_of :name, :prefix
+  validates_uniqueness_of :prefix
 
- # acts_as_paranoid
-
- # attr_protected :prefix
-
- # validates :prefix, :uniqueness => :true, :presence => :true
-
- has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :logo, :path => ":rails_root/public/system/:attachment/:id/:style/:filename", :url => "/system/:attachment/:id/:style/:filename"
 
 end
