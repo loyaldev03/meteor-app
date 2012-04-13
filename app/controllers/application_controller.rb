af3d566243fa42a6c4f18622347c9a0bd8837438
Carla Ares <carla.ares@honou.com.ar>
2012-04-13 10:37:55 -0300

    we must valdate partner prefix only if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,18 @@ class ApplicationController < ActionController::Base
   private
 
     def validate_partner_presence
-      if params[:partner_prefix].nil? and not params[:controller].include?('admin/')
-        flash[:error] = "No partner was selected."
-        redirect_to admin_partners_path
-        false
-      elsif not params[:partner_prefix].nil?
-        @current_partner = Partner.find_by_prefix(params[:partner_prefix])
-        if @current_partner.nil?
+      if current_user
+        if params[:partner_prefix].nil? and not params[:controller].include?('admin/')
           flash[:error] = "No partner was selected."
           redirect_to admin_partners_path
           false
+        elsif not params[:partner_prefix].nil?
+          @current_partner = Partner.find_by_prefix(params[:partner_prefix])
+          if @current_partner.nil?
+            flash[:error] = "No partner was selected."
+            redirect_to admin_partners_path
+            false
+          end
         end
       end
     end
