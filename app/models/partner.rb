9172911035cf11b64256ed0d0a45fe830f17d6f4
sebastian <sebastian@xagax.com>
2012-04-13 11:52:43 -0300

    #18333 - Added style to attached file

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -7,6 +7,8 @@ class Partner < ActiveRecord::Base
   validates_presence_of :name, :prefix
   validates_uniqueness_of :prefix, :name
 
-  has_attached_file :logo, :path => ":rails_root/public/system/:attachment/:id/:style/:filename", :url => "/system/:attachment/:id/:style/:filename"
-
+  has_attached_file :logo, :path => ":rails_root/public/system/:attachment/:id/:style/:filename", 
+                           :url => "/system/:attachment/:id/:style/:filename",
+                           :style => {:thumb=> "100x100#", :small  => "150x150>"}
+                           
 end
