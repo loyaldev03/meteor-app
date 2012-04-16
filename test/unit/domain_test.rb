4fe38596c465a7a98dcc500f1ced697adec4cf60
sebastian <sebastian@xagax.com>
2012-04-16 09:55:26 -0300

    #18349 - Added test for domain

--- test/unit/domain_test.rb
+++ test/unit/domain_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class DomainTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "Domain should not be created without a partner_id" do
+		domain = FactoryGirl.build(:domain)
+		assert !domain.save 
+	end
+
+
 end
