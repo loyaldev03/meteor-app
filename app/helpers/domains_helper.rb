5572b4d58f3062ce44cc8c420f6086b27204d639
sebastian <sebastian@xagax.com>
2012-04-16 10:45:58 -0300

    #18352 - Added helper to select club as select_tag

--- app/helpers/domains_helper.rb
+++ app/helpers/domains_helper.rb
@@ -3,6 +3,11 @@ module DomainsHelper
   def generate_select_partners_id (domain,partner)
     select_tag :partner_id, options_from_collection_for_select(partner, "id", "name")
   end
+
+  def generate_select_club_id (domain,club)
+  	select_tag :club_id, options_from_collection_for_select(club, "id", "name")
+  end
+
 end
 
 
