6e7854f93fc77309d6429b88e44ec555b8e06052
sebastian <sebastian@xagax.com>
2012-04-17 10:56:47 -0300

    #18352 - Fix test por uniqueness username

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   test "Shouldnt be two users with samen name" do
-    first = FactoryGirl.create(:user, :name => 'billy')
+    first = FactoryGirl.create(:user, :username => 'billy')
     assert first.valid?
-    second = FactoryGirl.build(:user, :name => 'billy')
+    second = FactoryGirl.build(:user, :username => 'billy')
     second.valid?
     assert_not_nil second.errors
   end
