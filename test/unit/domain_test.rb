233ce86aaca6fe96008ba97d7ea25ef8a969d724
sebastian <sebastian@xagax.com>
2012-04-19 11:58:58 -0300

    #18351 - Added test for url uniqueness

--- test/unit/domain_test.rb
+++ test/unit/domain_test.rb
@@ -2,24 +2,29 @@ require 'test_helper'
 
 class DomainTest < ActiveSupport::TestCase
 
-	test "Domain should not be created without a partner_id" do
-		domain = FactoryGirl.build(:domain)
-		assert !domain.save, "Domain was saved without a partner_id"
-	end
-
-	test "Domain should not be create without a club" do
-		domain = FactoryGirl.build(:domain)
+	test "Domain should not be create without a url" do
+		domain = FactoryGirl.build(:domain, :url => nil)
 		domain.club_id = nil
 		assert !domain.save, "Domain was saved without a club_id"
 	end
 
 	test "Domain shouldnt be destroyed is its the last one" do
 		partner = FactoryGirl.create(:partner)
-		first_domain = FactoryGirl.create(:domain, :partner_id => partner.id)
-		second_domain = FactoryGirl.create(:domain, :partner_id => partner.id)
-        
+		first_domain = FactoryGirl.create(:domain, :partner_id => partner.id, :url => 'http://prueba.com')
+		second_domain = FactoryGirl.create(:domain, :partner_id => partner.id, :url => 'http://prueba2.com')
+
         first_domain.destroy
         assert !second_domain.destroy, "Domain was destroyed when it was the last one"     
+        puts
 	end
+
+	test "Should not save two domains with the same url" do
+		domain = FactoryGirl.create(:domain, :url => 'http://xagax.com.ar')
+		assert domain.valid?
+		second_domain = FactoryGirl.build(:domain, :url => 'http://xagax.com.ar')
+		second_domain.valid?
+  	    assert_not_nil second_domain.errors
+	end	
+
 
 end
