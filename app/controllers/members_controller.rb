10f73a7f971ee88d19c1ae49212d0da8460a4dd8
sebastian <sebastian@xagax.com>
2012-04-20 15:36:51 -0300

    #18533 - Added form for members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -12,6 +12,15 @@ class MembersController < ApplicationController
   def show
   end
 
+  def new
+    @member = Member.new 
+    
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @domain }
+    end
+  end
+
   private
     def setup_member
       @member = Member.find_by_visible_id_and_club_id(params[:id], @current_club.id)
