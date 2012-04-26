a7b737a28aef2693f807678e8b88fb64babdee9f
sebastian <sebastian@xagax.com>
2012-04-26 14:57:36 -0300

    Added redirect at moment of creating new member

--- app/models/member.rb
+++ app/models/member.rb
@@ -96,7 +96,8 @@ class Member < ActiveRecord::Base
       # if amount.to_f == 0.0 => TODO: we should activate this member!!!!
       message = "Member enrolled successfully $#{amount}"
       Auditory.audit(agent, self, message, self)
-      { :message => message, :code => "000", :member_id => self.id }
+      self.reload
+      { :message => message, :code => "000", :member_id => self.id, :v_id => self.visible_id }
     rescue Exception => e
       # TODO: Notify devels about this!
       # TODO: this can happend if in the same time a new member is enrolled that makes this
