fff7073331f30ccffbe3a62580b67256e2487b7a
Carla Ares <carla.ares@honou.com.ar>
2012-04-27 09:57:09 -0300

    adding doc on Auditory method

--- lib/auditory.rb
+++ lib/auditory.rb
@@ -1,6 +1,11 @@
 class Auditory
+  # current_agent : if null will find the "batch" agent used on scripts.
+  # object : the object added/modify/deleted by agent
+  # description : custom message 
+  # member : member that must show this operation (only for operations that are related to members: e.g. CC management, Emails)
   def self.audit(current_agent, object, description, member = nil)
     begin
+      current_agent = Agent.find_by_email('batch@xagax.com.ar') if current_agent.nil?
       o = Operation.new :created_by_id => current_agent.id, :operation_date => DateTime.now, 
         :resource => object, :description => description
       o.member = member
