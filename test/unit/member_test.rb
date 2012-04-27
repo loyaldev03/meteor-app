f4538ce3f4d8d2e1d004362621e7c96cb582bd5e
sebastian <sebastian@xagax.com>
2012-04-27 14:36:54 -0300

    Added tests

--- test/unit/member_test.rb
+++ test/unit/member_test.rb
@@ -2,19 +2,39 @@ require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
   
-  # test "Should create a member" do
-  # 	member = FactoryGirl.build(:member)
-  # 	assert member.save
-  # end
+  test "Should create a member" do
+  	member = FactoryGirl.build(:member)
+  	assert member.save
+  end
 
-  # test "Should not create a member without first name" do
-  #   member = FactoryGirl.build(:member, :first_name => nil)
-  #   assert !member.save
-  # end
+  test "Should not create a member without first name" do
+    member = FactoryGirl.build(:member, :first_name => nil)
+    assert !member.save
+  end
 
   test "Should not create a member without last name" do
     member = FactoryGirl.build(:member, :last_name => nil)
   	assert !member.save
   end
+
+  test "Should not save with an invalid email" do
+    member = FactoryGirl.build(:member, :email => 'testing.com.ar')
+    member.valid?
+    assert_not_nil member.errors, member.errors.full_messages.inspect
+  end
+
+  test "Should not be two members with the same email within the same club" do
+    member = FactoryGirl.create(:member)
+    member_two = FactoryGirl.build(:member)
+    member_two.valid?
+    assert_not_nil member_two, member_two.errors.full_messages.inspect
+  end
+
+  test "Should let save two members with the same email in diferents clubs" do
+    member = FactoryGirl.create(:member, :club_id => 30)
+    member_two = FactoryGirl.build(:member)
+    assert member_two.save
+  end
+
 
 end
