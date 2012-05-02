56f6a140fae852b5d886852b38f5aaa3f98bd255
sebastian <sebastian@xagax.com>
2012-05-02 12:14:43 -0300

    removed status attribute

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,18 +2,9 @@ class Operation < ActiveRecord::Base
   belongs_to :created_by, :class_name => 'Agent', :foreign_key => 'created_by_id'
   belongs_to :resource, :polymorphic => true
   belongs_to :member
-  attr_accessible :description, :created_by_id, :operation_date, :resource
+  attr_accessible :description, :created_by_id, :operation_date, :resource, :notes
 
   before_create :set_operation_date_if_nil
-
-  state_machine :status, :initial => :completed do
-    # This operation cant have a button to do generate another operation. 
-    # E.g: Billing operation that was already refunded; Update profile
-    state :completed
-    # Operation that can have a button to do another related operation.
-    # E.g: Billing operation that CAN be refunded
-    state :modifiable
-  end
 
   private
     def set_operation_date_if_nil
