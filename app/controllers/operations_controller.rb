709eae34a46e17c74ad4cb4b13636ac26144ff06
sebastian <sebastian@xagax.com>
2012-05-02 13:37:52 -0300

    ref#18776 - Added audit when operation notes are updated.

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -2,12 +2,6 @@ class OperationsController < ApplicationController
   before_filter :validate_member_presence
   layout '2-cols'
 
-  # GET /domains
-  # GET /domains.json
-  def index
-
-  end
-
   # GET /domains/1
   # GET /domains/1.json
   def show
@@ -19,20 +13,6 @@ class OperationsController < ApplicationController
     end
   end
 
-  # GET /domains/new
-  # GET /domains/new.json
-  def new
-  end
-
-  # GET /domains/1/edit
-  def edit
-  end
-
-  # POST /domains
-  # POST /domains.json
-  def create
-  end
-
   # PUT /domains/1
   # PUT /domains/1.json
   def update
@@ -40,7 +20,10 @@ class OperationsController < ApplicationController
 
     respond_to do |format|
       if @operation.update_attributes(params[:operation])
-        format.html { redirect_to operation_path(:id => @operation), notice: "The operation was successfully updated." }
+        message = "The operation was successfully updated."
+        Auditory.audit(nil, @operation, message, @operation)
+        { :message => message, :code => "000", :operation_id => @operation.id
+        format.html { redirect_to operation_path(:id => @operation), notice: message }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -49,8 +32,4 @@ class OperationsController < ApplicationController
     end
   end
 
-  # DELETE /domains/1
-  # DELETE /domains/1.json
-  def destroy
-  end
 end
