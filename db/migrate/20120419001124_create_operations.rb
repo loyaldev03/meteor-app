60bca0c683d65f1ef371c2d857e33e715c18608a
Carla Ares <carla.ares@honou.com.ar>
2012-05-02 09:14:51 -0300

    changes on operations model

--- db/migrate/20120419001124_create_operations.rb
+++ db/migrate/20120419001124_create_operations.rb
@@ -5,7 +5,8 @@ class CreateOperations < ActiveRecord::Migration
       t.text :description
       t.datetime :operation_date
       t.integer :created_by_id
-      t.references :resource, :polymorphic => true
+      t.string :resource_type
+      t.string :resource_id
       t.timestamps
     end
     execute "ALTER TABLE operations ADD COLUMN id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY;" 
