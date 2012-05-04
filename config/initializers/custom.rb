3c2a273f2d7fe0478be7db906149223f82a77bcf
Carla Ares <carla.ares@honou.com.ar>
2012-05-04 14:41:43 -0300

    find in batches patch to suport uuid primary keys on members

--- config/initializers/custom.rb
+++ config/initializers/custom.rb
@@ -1,4 +1,7 @@
 require 'auditory'
+require 'clean_find_in_batches'
 
 ActiveMerchant::Billing::MerchantESolutionsGateway.wiredump_device = File.open("#{Rails.root}/log/active_merchant.log", "a+")  
 ActiveMerchant::Billing::MerchantESolutionsGateway.wiredump_device.sync = true
+
+ActiveRecord::Batches.send(:include, CleanFindInBatches)
