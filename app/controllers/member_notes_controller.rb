2cad1b2f08d38513165769f92e1dd365f82f9963
sebastian <sebastian@xagax.com>
2012-05-09 11:10:27 -0300

    ref#18537 - Added audition on member note creation

--- app/controllers/member_notes_controller.rb
+++ app/controllers/member_notes_controller.rb
@@ -12,10 +12,12 @@ class MemberNotesController < ApplicationController
     member_note = MemberNote.new(params[:member_note])
     member_note.created_by_id = @current_agent.id
     member_note.member_id = @current_member.id
-
+    message = "Agent #{@current_agent.username} added a note."
+    
     respond_to do |format|
       if member_note.save
-        format.html { redirect_to show_member_path, notice: "The note was added." }
+        Auditory.audit(@current_agent, member_note, message, @current_member)
+        format.html { redirect_to show_member_path, notice: "The note was added successfuly" }
         format.json { render json: @member_note, status: :created, location: @member_note }
       else
         format.html { render action: "new" }
