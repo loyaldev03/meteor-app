d897658d1fb5e0a97f9bf0f40b8b76144b70d53e
sebastian <sebastian@xagax.com>
2012-05-10 10:19:54 -0300

    ref#18537 - Fixed problems and syntaxis. Also added the back button on view.

--- app/controllers/member_notes_controller.rb
+++ app/controllers/member_notes_controller.rb
@@ -4,35 +4,23 @@ class MemberNotesController < ApplicationController
 
   def new
     @member_note = MemberNote.new
-    @disposition_type = DispositionType.find_by_club_id(@current_club.id)
-    @communication_type = CommunicationType.all
+    @disposition_types = DispositionType.find_all_by_club_id(@current_club.id)
+    @communication_types = CommunicationType.all
   end
 
   def create 
     member_note = MemberNote.new(params[:member_note])
     member_note.created_by_id = @current_agent.id
     member_note.member_id = @current_member.id
-    message = "Agent #{@current_agent.username} added a note."
+    message = "Note added."
     
     respond_to do |format|
       if member_note.save
         Auditory.audit(@current_agent, member_note, message, @current_member)
         format.html { redirect_to show_member_path, notice: "The note was added successfuly" }
-        format.json { render json: @member_note, status: :created, location: @member_note }
       else
         format.html { render action: "new" }
-        format.json { render json: @member_note.errors, status: :unprocessable_entity }
       end
     end
   end
-
-
-
-
-
-
-
-
-
-
 end
