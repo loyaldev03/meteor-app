e2ec7064d716756083a8513cc324bfc22b143fc2
sebastian <sebastian@xagax.com>
2012-05-10 09:23:02 -0300

    ref#18914 - Fixed problem to mass assign disposition_type

--- app/models/member_note.rb
+++ app/models/member_note.rb
@@ -1,5 +1,5 @@
 class MemberNote < ActiveRecord::Base
-  attr_accessible :communication_type_id, :created_by_id, :member_id, :note_type, :description
+  attr_accessible :communication_type_id, :created_by_id, :member_id, :note_type, :description, :disposition_type_id
   belongs_to :created_by, :class_name => 'Agent', :foreign_key => 'created_by_id'
   belongs_to :member
   belongs_to :disposition_type
