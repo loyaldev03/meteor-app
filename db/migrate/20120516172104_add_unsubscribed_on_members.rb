26e37566957bc2827021227d9b5d9e034804db9f
Carla Ares <carla.ares@honou.com.ar>
2012-05-16 14:31:21 -0300

    changin email_unsubscribed column type. Feature refs #19159

--- db/migrate/20120516172104_add_unsubscribed_on_members.rb
+++ db/migrate/20120516172104_add_unsubscribed_on_members.rb
@@ -1,9 +1,9 @@
 class AddUnsubscribedOnMembers < ActiveRecord::Migration
   def up
-    add_column :members, :email_unsubscribed, :boolean, :default => false
+    add_column :members, :email_unsubscribed_at, :date
   end
 
   def down
-    remove_column :members, :email_unsubscribed
+    remove_column :members, :email_unsubscribed_at
   end
 end
