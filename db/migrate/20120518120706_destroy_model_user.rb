6b34b736170724fb67a953c2f87250a6880fd5df
Carla Ares <carla.ares@honou.com.ar>
2012-05-18 09:07:58 -0300

    removing user model, wont be use on this release

---
+++ db/migrate/20120518120706_destroy_model_user.rb
@@ -0,0 +1,19 @@
+class DestroyModelUser < ActiveRecord::Migration
+  def down
+    create_table :users, {:id => false} do |t|
+      t.string :uuid, :primary => true
+      t.string :ip_address
+      t.string :user_agent
+      t.string :referer_host
+      t.string :referer_path
+      t.text :referer_params
+      t.boolean :cookie_set
+      t.integer :domain_id, :limit => 8
+      t.timestamps
+    end
+  end
+  def up
+    drop_table :users
+  end
+
+end
