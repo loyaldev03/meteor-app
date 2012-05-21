5d3d5369553da4ceeb91b9ef6c79c47b0f9a2758
Ignacio Carrera <carreraignacio@gmail.com>
2012-05-21 09:43:17 -0300

    smal fix in factories

--- test/factories/member.rb
+++ test/factories/member.rb
@@ -5,14 +5,14 @@ FactoryGirl.define do
     # but test database is not created by migrations, so our custom executes
     # are not used. Consequence: visible_id is not auto increment.
     sequence(:visible_id) {|n| n }
-    first_name Faker::Name.first_name 
-    last_name Faker::Name.last_name
-    address Faker::Address.street_address 
-    city Faker::Address.city
-    zip Faker::Address.zip
-    state Faker::Address.us_state
+    first_name { Faker::Name.first_name  }
+    last_name { Faker::Name.last_name }
+    address { Faker::Address.street_address  }
+    city { Faker::Address.city }
+    zip { Faker::Address.zip }
+    state { Faker::Address.us_state }
     sequence(:email) {|n| "member#{n}@test.no" }
-    phone_number Faker::PhoneNumber.phone_number
+    phone_number { Faker::PhoneNumber.phone_number }
     country "US"
     association :club
     association :terms_of_membership
@@ -22,14 +22,14 @@ FactoryGirl.define do
   factory :paid_member, class: Member do
     sequence(:visible_id) {|n| n }
     status "paid"
-    first_name Faker::Name.first_name 
-    last_name Faker::Name.last_name
-    address Faker::Address.street_address 
-    city Faker::Address.city
-    zip Faker::Address.zip
-    state Faker::Address.us_state
+    first_name { Faker::Name.first_name  }
+    last_name { Faker::Name.last_name }
+    address { Faker::Address.street_address  }
+    city { Faker::Address.city }
+    zip { Faker::Address.zip }
+    state { Faker::Address.us_state }
     sequence(:email) {|n| "member#{n}@test.no" }
-    phone_number Faker::PhoneNumber.phone_number
+    phone_number { Faker::PhoneNumber.phone_number }
     country "US"
     association :club
     association :terms_of_membership, factory: :terms_of_membership_insuf_funds
@@ -39,14 +39,14 @@ FactoryGirl.define do
   factory :lapsed_member, class: Member do
     sequence(:visible_id) {|n| n }
     status "lapsed"
-    first_name Faker::Name.first_name 
-    last_name Faker::Name.last_name
-    address Faker::Address.street_address 
-    city Faker::Address.city
-    zip Faker::Address.zip
-    state Faker::Address.us_state
+    first_name { Faker::Name.first_name  }
+    last_name { Faker::Name.last_name }
+    address { Faker::Address.street_address  }
+    city { Faker::Address.city }
+    zip { Faker::Address.zip }
+    state { Faker::Address.us_state }
     sequence(:email) {|n| "member#{n}@test.no" }
-    phone_number Faker::PhoneNumber.phone_number
+    phone_number { Faker::PhoneNumber.phone_number }
     country "US"
     association :club
     association :terms_of_membership
@@ -55,14 +55,14 @@ FactoryGirl.define do
   factory :provisional_member, class: Member do
     sequence(:visible_id) {|n| n }
     status "provisional"
-    first_name Faker::Name.first_name 
-    last_name Faker::Name.last_name
-    address Faker::Address.street_address 
-    city Faker::Address.city
-    zip Faker::Address.zip
-    state Faker::Address.us_state
+    first_name { Faker::Name.first_name  }
+    last_name { Faker::Name.last_name }
+    address { Faker::Address.street_address  }
+    city { Faker::Address.city }
+    zip { Faker::Address.zip }
+    state { Faker::Address.us_state }
     sequence(:email) {|n| "member#{n}@test.no" }
-    phone_number Faker::PhoneNumber.phone_number
+    phone_number { Faker::PhoneNumber.phone_number }
     country "US"
     join_date { DateTime.now }
     next_retry_bill_date { DateTime.now } 
