2edf308eb2b35122e36312bb351bf03df9c0e778
sebastian <sebastian@xagax.com>
2012-05-23 15:16:51 -0300

    added test on credit card.

--- test/unit/credit_card_test.rb
+++ test/unit/credit_card_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CreditCardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	test "Should not let activate a blacklisted credit card" do
+	  credit_card_one = FactoryGirl.create(:credit_card)
+      credit_card_two = FactoryGirl.create(:credit_card_master_card)
+      credit_card_one.blacklisted = 1
+      credit_card_one.active = 1
+      assert !credit_card_one.save, "blacklisted credit card activated. #{credit_card_two.errors.inspect}"
+	end
+
 end
