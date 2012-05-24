427db31f7993e37f1a445a604676e401985d76d3
sebastian <sebastian@xagax.com>
2012-05-24 10:21:34 -0300

    Fixed validation on credit card.

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -9,7 +9,7 @@ class CreditCard < ActiveRecord::Base
   validates :expire_month, :numericality => { :only_integer => true, :greater_than => 0, :less_than_or_equal_to => 12 }
   validates :expire_year, :numericality => { :only_integer => true, :greater_than => 2000 } 
   validates :number, :numericality => true
-  validates :active, :is_not_blacklisted => true
+  before_update :not_blacklisted
 
   def accepted_on_billing
     update_attribute :last_successful_bill_date, DateTime.now
@@ -80,4 +80,12 @@ class CreditCard < ActiveRecord::Base
     end
     acc
   end
+
+  def not_blacklisted
+    if self.blacklisted? && self.active?
+      errors.add(:active, "Cannot be activated. Its blacklisted") 
+      false
+    end
+  end
+  
 end
