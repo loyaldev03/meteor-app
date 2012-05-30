a953aa76f65cf397cade1d379c0f69ee90856359
sebastian <sebastian@xagax.com>
2012-05-29 21:00:43 -0300

    ref#19217 - Member search improved.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,28 +4,18 @@ class MembersController < ApplicationController
 
   def index
     if request.post?
-      unless params[:member][:last_digits].blank?
-        @members = Member.joins(:credit_cards).where([" visible_id like ? AND first_name like ? AND last_name like ? 
-                     AND address like ? AND phone_number like ? AND city like ? AND state like ? AND zip like ? AND email like ? 
-                     AND (bill_date like ? OR bill_date is null) AND club_id like ? AND credit_cards.active = 1 
-                     AND credit_cards.last_digits = ?", 
-                   '%'+params[:member][:member_id]+'%','%'+params[:member][:first_name]+'%',
-                   '%'+params[:member][:last_name]+'%','%'+params[:member][:address]+'%',
-                   '%'+params[:member][:phone_number]+'%','%'+params[:member][:city]+'%',
-                   '%'+params[:member][:state]+'%','%'+params[:member][:zip]+'%', 
-                   '%'+params[:member][:email]+'%','%'+params[:member][:bill_date]+'%', @current_club,
-                   params[:member][:last_digits]])
-      else
-        @members = Member.joins(:credit_cards).where([" visible_id like ? AND first_name like ? 
-                     AND last_name like ? AND address like ? AND phone_number like ? AND city like ? 
-                     AND state like ? AND zip like ? AND email like ? AND (bill_date like ? OR bill_date is null) 
-                     AND club_id like ?",
-                   '%'+params[:member][:member_id]+'%','%'+params[:member][:first_name]+'%',
-                   '%'+params[:member][:last_name]+'%','%'+params[:member][:address]+'%',
-                   '%'+params[:member][:phone_number]+'%','%'+params[:member][:city]+'%',
-                   '%'+params[:member][:state]+'%','%'+params[:member][:zip]+'%',
-                   '%'+params[:member][:email]+'%','%'+params[:member][:bill_date]+'%',@current_club])             
-      end
+      params[:member][:member_id].blank? ? member_id = '%' : member_id = params[:member][:member_id]
+      params[:member][:last_digits].blank? ? last_digits = '%' : last_digits = params[:member][:last_digits]
+      @members = Member.joins(:credit_cards).where(["visible_id like ? AND first_name like ? AND last_name like ? 
+                 AND address like ? AND phone_number like ? AND city like ? AND state like ? AND zip like ? AND email like ? 
+                 AND (bill_date like ? OR bill_date is null) AND club_id like ? AND (credit_cards.active = 1 
+                 AND credit_cards.last_digits like ?)", 
+                 member_id,'%'+params[:member][:first_name]+'%',
+                 '%'+params[:member][:last_name]+'%','%'+params[:member][:address]+'%',
+                 '%'+params[:member][:phone_number]+'%','%'+params[:member][:city]+'%',
+                 '%'+params[:member][:state]+'%','%'+params[:member][:zip]+'%', 
+                 '%'+params[:member][:email]+'%','%'+params[:member][:bill_date]+'%', @current_club,
+                 last_digits])
    end
   end
 
