23843f7faf72ff7d42e8e64075a0db07b6d31b81
sebastian <sebastian@xagax.com>
2012-06-04 10:31:59 -0300

    fixed migration

--- db/migrate/20120601175952_create_club_cash_transaction.rb
+++ db/migrate/20120601175952_create_club_cash_transaction.rb
@@ -1,6 +1,6 @@
 class CreateClubCashTransaction < ActiveRecord::Migration
   def up
-  	create_table :club_cash_transcation do |t|
+  	create_table :club_cash_transcations do |t|
       t.string   :member_id
       t.decimal  :amount, :default => 0.0      
       t.text     :description
@@ -8,6 +8,6 @@ class CreateClubCashTransaction < ActiveRecord::Migration
   end
 
   def down
-  	drop_table :club_cash_transcation
+  	drop_table :club_cash_transcations
   end
 end
