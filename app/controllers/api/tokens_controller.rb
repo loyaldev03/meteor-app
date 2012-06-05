be12bf761efc41470698258f2c14771188b9e403
Carla Ares <carla.ares@honou.com.ar>
2012-06-05 10:21:32 -0300

    invalid characters con api controller

--- app/controllers/api/tokens_controller.rb
+++ app/controllers/api/tokens_controller.rb
@@ -39,8 +39,8 @@ class Api::TokensController < ApplicationController
   def destroy
     @user=Agent.find_by_authentication_token(params[:id])
     if @user.nil?
-      logger.info(“Token not found.”)
-      render :status=>404, :json=>{:message=>”Invalid token.”}
+      logger.info("Token not found.")
+      render :status=>404, :json=>{:message=>"Invalid token."}
     else
       @user.reset_authentication_token!
       render :status=>200, :json=>{:token=>params[:id]}
