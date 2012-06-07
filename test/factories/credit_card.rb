443e2511b74976cff0b3c4e9da8af972d64f96c7
Carla Ares <carla.ares@honou.com.ar>
2012-06-07 10:52:55 -0300

    CC month and year test must be dynamic

--- test/factories/credit_card.rb
+++ test/factories/credit_card.rb
@@ -2,15 +2,15 @@ FactoryGirl.define do
 
   factory :credit_card do
     active true
-    expire_month 12
-    expire_year 2013
+    expire_month { (Date.today + 1.month).month }
+    expire_year { (Date.today + 1.year).year }
     number "4012301230123010"
   end
 
   factory :credit_card_master_card, class: CreditCard do
     active true
-    expire_month 12
-    expire_year 2013
+    expire_month { (Date.today + 1.month).month }
+    expire_year { (Date.today + 1.year).year }
     number "5589548939080095"
   end
 
