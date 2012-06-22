01f3bb4280a0b3b9962add0fba8a25a90ac8ca03
Ignacio Carrera <carreraignacio@gmail.com>
2012-06-22 18:41:55 -0300

    fix support for api/prospects

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,11 +1,14 @@
 class Prospect < ActiveRecord::Base
-  include Extensions::UUID
+  # include Extensions::UUID
+  include Extensions::SerializeJson
 
-  serialize :preferences, JSON
+  serialize_json :preferences,
+    :referral_parameters,
+    :mega_channel
 
-  attr_accessible :first_name, :last_name, :address, :city, :state, :zip, :email, :phone, :birth_date,
+  attr_accessible :first_name, :last_name, :address, :city, :state, :zip, :email, :phone_number, :birth_date,
                   :preferences, :mega_chanel, :ip_address, :referral_host, :referral_parameters, :cookie_value,
-                  :reporting_code, :product_id, :user_id, :url_landing
+                  :reporting_code, :product_id, :user_id, :url_landing, :mega_channel, :user_agent, :joint
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ SacPlatform::Application.routes.draw do
       match 'profile' => 'members#profile', as: 'v1_get_member_profile', :via => :get
       match 'enroll' => 'members#enroll', as: 'v1_enroll_members', :via => :post
       match 'update_profile/:id/:club_id' => 'members#update_profile', as: 'v1_update_profile_members', :via => :put
+      post 'prospects' => 'prospects#enroll'
     end
   end
 
