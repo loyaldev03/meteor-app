e0a31c47e70e5babcb25bb0930703cc62a847ec1
Carla Ares <carla.ares@honou.com.ar>
2012-06-25 14:05:56 -0300

    refs #21009 - adding pre bill renewal email template type. It will be implemented on next version.

--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -2,7 +2,7 @@ class EmailTemplate < ActiveRecord::Base
   belongs_to :terms_of_membership
   serialize :external_attributes
 
-  TEMPLATE_TYPES =  [ :welcome, :active, :cancellation, :prebill, :refund ]
+  TEMPLATE_TYPES =  [ :welcome, :active, :cancellation, :prebill, :prebill_renewal, :refund ]
 
   CLIENTS = [ :amazon, :action_mailer, :lyris ]
 
