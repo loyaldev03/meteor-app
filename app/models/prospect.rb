8b1a815ce4dadee55ba035d4d81f74eccc58ebf3
sebastian <sebastian@xagax.com>
2012-07-02 10:18:57 -0300

    removed from attr_accesible attributes that were no longer in use

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -6,7 +6,7 @@ class Prospect < ActiveRecord::Base
 
   attr_accessible :first_name, :last_name, :address, :city, :state, :zip, :email, :phone_number, :birth_date,
                   :preferences, :mega_chanel, :ip_address, :referral_host, :referral_parameters, :cookie_value,
-                  :reporting_code, :product_id, :user_id, :url_landing, :mega_channel, :user_agent, :joint
+                  :marketing_code, :product_sku, :user_id, :url_landing, :mega_channel, :user_agent, :joint
 
 
 end
