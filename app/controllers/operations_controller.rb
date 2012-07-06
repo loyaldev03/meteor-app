49687e964fa11ba1c501d8293ca6388d45afe2c7
Carla Ares <carla.ares@honou.com.ar>
2012-07-06 19:25:35 -0300

    minimal changes on visual stuff

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,7 +1,6 @@
 class OperationsController < ApplicationController
   before_filter :validate_club_presence
   before_filter :validate_member_presence
-  layout '2-cols'
 
   def index
     if request.post?
