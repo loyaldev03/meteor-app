aa5dcc3368c32440316a4a8346cefdd645edd5dc
sebastian <sebastian@xagax.com>
2012-07-10 10:28:48 -0300

    ref#21267 - Added feature to check if member belongs to a group or not (and according to that) which amount we will be adding on assign club cash method

--- app/models/member.rb
+++ app/models/member.rb
@@ -493,7 +493,8 @@ class Member < ActiveRecord::Base
 
   # Adds club cash when enrolling. It calls "add_club_cash" method
   def assign_club_cash!(message = "Adding club cash on new enrollment.")
-    self.add_club_cash(nil, terms_of_membership.club_cash_amount, message)
+    self.member_group_type_id ? amount = Settings.club_cash_for_members_who_belongs_to_group : amount= terms_of_membership.club_cash_amount
+    self.add_club_cash(nil, amount, message)
     self.club_cash_expire_date = self.join_date + 1.year
     self.save!
   end
