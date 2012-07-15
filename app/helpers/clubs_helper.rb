987882ea6522add8d622becf99b059029169be80
claudio <claudio@claudio-desktop.(none)>
2012-07-15 12:01:38 -0300

    changes in the themes

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -1,7 +1,7 @@
 module ClubsHelper
 
   def theme_switcher(f)
-    f.select :theme, ['application', 'theme1', 'theme2', 'theme3']
+    f.select :theme, ['application', 'readable', 'united']
   end
 
 end
