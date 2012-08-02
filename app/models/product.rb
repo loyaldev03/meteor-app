4fcff4430aec47fcdabaf35c701783254599f65c
sebastian <sebastian@xagax.com>
2012-08-02 14:40:59 -0300

    ref#21745 - Added model product with some validations.

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ActiveRecord::Base
+  attr_accessible :club_id, :name, :recurrent, :sku, :stock, :weight
+
+  validates :sku, :uniqueness => true, :pressence => true
+  validates :stock, :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :sku, :format => /^[a-zA-Z -_]$/
+end
