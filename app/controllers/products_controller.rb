5c653deb64197dfe5cb89dad7e6d30babd9daeab
Carla Ares <carla.ares@honou.com.ar>
2012-08-06 21:09:11 -0300

    removing format html when its not necesarilly. refs #21745

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,6 @@ class ProductsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: ProductsDatatable.new(view_context,@current_partner,@current_club) }
-
     end
   end
 
@@ -16,19 +15,12 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @product = Product.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
   end
 
   # GET /products/new
   # GET /products/new.json
   def new
     @product = Product.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-    end
   end
 
   # GET /products/1/edit
@@ -41,12 +33,10 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(params[:product])
     @product.club_id = @current_club.id
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to product_path(@current_partner.prefix,@current_club.name, @product), notice: 'Product was successfully created.' }
-      else
-        format.html { render action: "new" }
-      end
+    if @product.save
+      redirect_to product_path(@current_partner.prefix,@current_club.name, @product), notice: 'Product was successfully created.'
+    else
+      render action: "new"
     end
   end
 
@@ -55,12 +45,10 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
 
-    respond_to do |format|
-      if @product.update_attributes(params[:product])
-        format.html { redirect_to product_path(@current_partner.prefix,@current_club.name, @product), notice: 'Product was successfully created.' }
-      else
-        format.html { render action: "edit" }
-      end
+    if @product.update_attributes(params[:product])
+      redirect_to product_path(@current_partner.prefix,@current_club.name, @product), notice: 'Product was successfully created.' 
+    else
+      render action: "edit"
     end
   end
 
@@ -69,10 +57,7 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-
-    respond_to do |format|
-      format.html { redirect_to products_url }
-    end
+    redirect_to products_url 
   end
 
   def check_permissions
