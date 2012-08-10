6b6283ddabf5576ec8da104a7f3298875a780d0b
Ignacio Carrera <carreraignacio@gmail.com>
2012-08-10 09:45:43 -0300

    Product#sku validation was not accepting '-'

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ActiveRecord::Base
 
   validates :sku, :uniqueness => true, :presence => true
   validates :stock, :numericality => { :only_integer => true, :greater_than => 0 }
-  validates :sku, :format => /^[a-zA-Z-_]+$/
+  validates :sku, :format => /^[a-zA-Z\-_]+$/
 
 
   def self.datatable_columns
