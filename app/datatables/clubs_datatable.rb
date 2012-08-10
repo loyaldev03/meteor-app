a3311a0536159942b09567ce45f6fc18dd5b817f
sebastian <sebastian@xagax.com>
2012-08-10 10:30:59 -0300

    ref#21906-Added permissions on club's datatable.

--- app/datatables/clubs_datatable.rb
+++ app/datatables/clubs_datatable.rb
@@ -16,12 +16,12 @@ private
         club.name, 
         club.description,
         club.members.count,
-        link_to(I18n.t('activerecord.model.members'), @url_helpers.members_path(@current_partner.prefix, club.name), :class => 'btn btn-mini')+
-        link_to(I18n.t('activerecord.model.products'), @url_helpers.products_path(@current_partner.prefix, club.name), :class => 'btn btn-mini')+
-        link_to(I18n.t(:edit), @url_helpers.edit_club_path(:partner_prefix => @current_partner.prefix,:id => club.id), :class => 'btn btn-mini')+
-        link_to(I18n.t(:destroy), @url_helpers.club_path(:partner_prefix => @current_partner.prefix, :id => club.id), :method => :delete,
+        (link_to(I18n.t('activerecord.model.members'), @url_helpers.members_path(@current_partner.prefix, club.name), :class => 'btn btn-mini')if @current_agent.can? :read, Member)+
+        (link_to(I18n.t('activerecord.model.products'), @url_helpers.products_path(@current_partner.prefix, club.name), :class => 'btn btn-mini')if @current_agent.can? :read, Product )+
+        (link_to(I18n.t(:edit), @url_helpers.edit_club_path(:partner_prefix => @current_partner.prefix,:id => club.id), :class => 'btn btn-mini')if @current_agent.can? :update, Club)+
+        (link_to(I18n.t(:destroy), @url_helpers.club_path(:partner_prefix => @current_partner.prefix, :id => club.id), :method => :delete,
                         :confirm => I18n.t('.confirm', :default => I18n.t("helpers.links.confirm", :default => 'Are you sure?')),
-                        :class => 'btn btn-mini btn-danger')
+                        :class => 'btn btn-mini btn-danger')if @current_agent.can? :update, Club)
       ]
     end
   end
