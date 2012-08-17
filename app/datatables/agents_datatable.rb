525fcddd2dddd0deb334391160e01acb0884e9b9
sebastian <sebastian@xagax.com>
2012-08-17 16:06:08 -0300

    changed link_to show. Now it is show as a button

--- app/datatables/agents_datatable.rb
+++ app/datatables/agents_datatable.rb
@@ -14,11 +14,12 @@ private
   def data
     agents.map do |agent|
       [
-        link_to(agent.id, @url_helpers.admin_agent_path(agent)),
+        agent.id,
         agent.email, 
         agent.username,
         if (agent.locked_at); I18n.l(agent.locked_at,:format=>:long) ;end,
         I18n.l(agent.created_at,:format=>:long),
+        link_to(I18n.t(:show), @url_helpers.admin_agent_path(agent), :class => 'btn btn-mini')+' '+
         link_to(I18n.t(:edit), @url_helpers.edit_admin_agent_path(agent), :class => 'btn btn-mini' )+' '+
         link_to(I18n.t(:destroy), @url_helpers.admin_agent_path(agent), :method => :delete,
                         :confirm => I18n.t('.confirm', :default => I18n.t("helpers.links.confirm", :default => 'Are you sure?')),
