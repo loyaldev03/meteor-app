698c966fbdac445f1e3617653bcde0d426a55752
Carla Ares <carla.ares@honou.com.ar>
2012-08-21 21:25:18 -0300

    adding test for chargeback. but unsuccess, faraday and stubs not working for my sourcecode

--- app/models/payment_gateway_configuration.rb
+++ app/models/payment_gateway_configuration.rb
@@ -44,38 +44,42 @@ class PaymentGatewayConfiguration < ActiveRecord::Base
       :includeClientRefNum => true, 
       :dsReportId => 5
     }
-    lines = result.body.split("\n")
-    lines.each do |line|
-      columns = line.split(',')
-      next if columns[0].include?('Merchant Id')
-      columns.each { |x| x.gsub!('"', '') }
-      args = { :control_number => columns[2], :incomming_date => columns[3],
-        :reference_number => columns[5], :transaction_date => columns[6], :transaction_amount => columns[7],
-        :trident_transaction_id => columns[8], :purchase_transaction_id => columns[9],
-        :client_reference_number => columns[10], :auth_code => columns[11],
-        :adjudication_date => columns[12], :adjudication_number => columns[13],
-        :reason => columns[14], :first_time => columns[15],
-        :reason_code => columns[16], :cb_ref_number => columns[17]
-      }
-      begin
-        transaction_chargebacked = Transaction.find_by_payment_gateway_id_and_response_transaction_id self.id, args[:trident_transaction_id]
-        member = Member.find_by_visible_id_and_club_id(args[:client_reference_number], self.club_id)
-        if transaction_chargebacked.member_id == member.id
-          member.chargeback! transaction_chargebacked, args
-          member.save
-        else
-          raise "Chargeback ##{args[:control_number]} could not be processed. member and transaction_chargebacked are different! #{line}"
-        end
-      rescue Exception => e
-        Airbrake.notify(:error_class => "MES::chargeback_report", :error_message => "#{e.to_s}\n\n#{$@[0..9] * "\n\t"}")
-        Rails.logger.info "    [!] failed: #{$!.inspect}\n\t#{$@[0..9] * "\n\t"}"
-      end
-    end
+    process_mes_chargebacks_result(result.body) if result.success?
   end
 
   def self.process_mes_chargebacks(mode)
     PaymentGatewayConfiguration.find_all_by_gateway_and_mode('mes', mode).each do |gateway|
       gateway.process_mes_chargebacks
     end
-  end  
+  end
+
+  private
+    def process_mes_chargebacks_result(body)
+      body.split("\n").each do |line|
+        columns = line.split(',')
+        next if columns[0].include?('Merchant Id')
+        columns.each { |x| x.gsub!('"', '') }
+        args = { :control_number => columns[2], :incomming_date => columns[3],
+          :reference_number => columns[5], :transaction_date => columns[6], :transaction_amount => columns[7],
+          :trident_transaction_id => columns[8], :purchase_transaction_id => columns[9],
+          :client_reference_number => columns[10], :auth_code => columns[11],
+          :adjudication_date => columns[12], :adjudication_number => columns[13],
+          :reason => columns[14], :first_time => columns[15],
+          :reason_code => columns[16], :cb_ref_number => columns[17]
+        }
+        begin
+          transaction_chargebacked = Transaction.find_by_payment_gateway_id_and_response_transaction_id self.id, args[:trident_transaction_id]
+          member = Member.find_by_visible_id_and_club_id(args[:client_reference_number], self.club_id)
+          if transaction_chargebacked.member_id == member.id
+            member.chargeback! transaction_chargebacked, args
+            member.save
+          else
+            raise "Chargeback ##{args[:control_number]} could not be processed. member and transaction_chargebacked are different! #{line}"
+          end
+        rescue Exception => e
+          Airbrake.notify(:error_class => "MES::chargeback_report", :error_message => "#{e.to_s}\n\n#{$@[0..9] * "\n\t"}")
+          Rails.logger.info "    [!] failed: #{$!.inspect}\n\t#{$@[0..9] * "\n\t"}"
+        end
+      end
+    end      
 end
