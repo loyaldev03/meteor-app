b9a1ba846af42ce0f1f8586b59cb1397414ad624
claudio <claudio@claudio-desktop.(none)>
2012-08-28 22:46:37 -0300

    new bill integration test

--- test/factories/enrollment_info.rb
+++ test/factories/enrollment_info.rb
@@ -7,5 +7,15 @@ FactoryGirl.define do
     user_agent 'Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit\/536.11 (KHTML, like Gecko) Chrome\/20.0.1132.47 Safari\/536.11'
   end
 
+  factory :complete_enrollment_info_with_amount, class: EnrollmentInfo do
+    enrollment_amount = 0.5
+    product_sku 'Bracelet,kit-card'
+    ip_address '190.224.250.164'
+    campaign_medium 'xyz123456'
+    user_agent 'Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit\/536.11 (KHTML, like Gecko) Chrome\/20.0.1132.47 Safari\/536.11'
+  	mega_channel 'super channel'
+  end
+
+  #Create a member with enroll = 0, installment amount > 0 and provisional_days =0
 
 end
