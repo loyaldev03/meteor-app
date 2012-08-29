44d5a2ebe7525dcf8fb939bd4364003eaae9af35
Carla Ares <carla.ares@honou.com.ar>
2012-08-28 21:15:29 -0300

    email_unsubscribed_at does not exist? .. Removing it from migrations

--- db/migrate/20120828185516_change_from_date_to_datetime.rb
+++ db/migrate/20120828185516_change_from_date_to_datetime.rb
@@ -4,7 +4,6 @@ class ChangeFromDateToDatetime < ActiveRecord::Migration
     change_column :members, :cancel_date, :datetime
     change_column :members, :bill_date, :datetime
     change_column :members, :next_retry_bill_date, :datetime
-    change_column :members, :email_unsubscribed_at, :datetime
     change_column :members, :club_cash_expire_date, :datetime
   end
 
@@ -13,7 +12,6 @@ class ChangeFromDateToDatetime < ActiveRecord::Migration
     change_column :members, :cancel_date, :date
     change_column :members, :bill_date, :date
     change_column :members, :next_retry_bill_date, :date
-    change_column :members, :email_unsubscribed_at, :date
     change_column :members, :club_cash_expire_date, :date
   end
 end
