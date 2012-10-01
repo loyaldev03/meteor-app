5ab470a9f2969358257c7a8138da1bf98a562318
sebastian <sebastian@xagax.com>
2012-10-01 19:23:16 -0300

    ref#22786-added test for gender and type_of_phone_number validation.

--- app/models/member.rb
+++ app/models/member.rb
@@ -623,7 +623,7 @@ class Member < ActiveRecord::Base
     self.birth_date = params[:birth_date]
     self.joint = params[:joint]
     self.gender = params[:gender]
-    self.type_of_phone_number = params[:type_of_phone_number]
+    params[:type_of_phone_number].blank? ? self.type_of_phone_number=nil : self.type_of_phone_number = params[:type_of_phone_number]
     self.phone_country_code = params[:phone_country_code]
     self.phone_area_code = params[:phone_area_code]
     self.phone_local_number = params[:phone_local_number]
