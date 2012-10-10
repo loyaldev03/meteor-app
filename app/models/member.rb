10a667a5eada1958ef240df586da0a6171dc815d
sebastian <sebastian@xagax.com>
2012-10-10 09:47:06 -0300

    ref#22330-Fixed bug. Added integration test. Cancel date cannot be changed

--- app/models/member.rb
+++ app/models/member.rb
@@ -237,7 +237,7 @@ class Member < ActiveRecord::Base
 
   # Returns true if members is lapsed.
   def can_be_canceled?
-    !self.lapsed? or !self.cancel_date
+    !self.lapsed? and !self.cancel_date
   end
 
   # Returns true if member is applied. 
