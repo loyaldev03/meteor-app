99b9bfe525f6e5d42208aaccd02c22d1c54da9b4
sebastian <sebastian@xagax.com>
2012-10-22 20:43:31 -0300

    added downcase to type_of_phone_number to the field for sync.

--- lib/sac_drupal/models/member.rb
+++ lib/sac_drupal/models/member.rb
@@ -133,7 +133,7 @@ module Drupal
         },
         field_profile_phone_type: { 
           und: { 
-              select: m.type_of_phone_number
+              select: m.type_of_phone_number.downcase
             }
         },
         field_profile_phone_country_code: { 
