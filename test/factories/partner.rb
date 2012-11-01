6a226af6b948f6f34b662b9dbca4ccb2cde2dba0
sebastian <sebastian@xagax.com>
2012-11-01 13:38:15 -0300

    fixed functional tests

--- test/factories/partner.rb
+++ test/factories/partner.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :partner do
     prefix { Faker::Internet.domain_word.upcase+Faker::Internet.domain_word.upcase }
-    name { Faker::Internet.domain_word }
+    name { Faker::Internet.domain_word+Faker::Internet.domain_word }
     sequence(:description) {|n| "description_#{n}" }
  		contract_uri { "http://#{Faker::Internet.domain_name}" }
 		website_url { "http://#{Faker::Internet.domain_name}" }
