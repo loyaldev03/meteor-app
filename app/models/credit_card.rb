7de41a0a0cec913262eae7d6f49faa7bccef23bd
Carla Ares <carla.ares@honou.com.ar>
2012-11-14 10:56:03 -0300

    credit card reciclation wont create a new credit card record. It will only update existing record and will add an operation. test cases added.

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -87,21 +87,24 @@ class CreditCard < ActiveRecord::Base
     if acc.am_card.expired?
       case times
       when 0
-        new_year_exp=acc.expire_year.to_i + 3
+        new_year_exp=acc.expire_year += 3
       when 1
-        new_year_exp=acc.expire_year.to_i + 2
+        new_year_exp=acc.expire_year += 2
       when 2
-        new_year_exp=acc.expire_year.to_i + 4
+        new_year_exp=acc.expire_year += 4
       when 3
-        new_year_exp=acc.expire_year.to_i + 1
+        new_year_exp=acc.expire_year += 1
       when 4
-        new_year_exp=acc.expire_year.to_i + 6
+        new_year_exp=acc.expire_year += 6
       when 5
-        new_year_exp=acc.expire_year.to_i + 5
+        new_year_exp=acc.expire_year += 5
       else
-        new_year_exp=Time.zone.now.year
+        new_year_exp+=Time.zone.now.year
       end
-      return CreditCard.new_expiration_on_active_credit_card(acc, new_year_exp) || acc
+      if new_year_exp != acc.expire_year.to_i
+        Auditory.audit(nil, cc, "Automatic Recycled Expired card from #{acc.expire_month}/#{acc.expire_year} to #{acc.expire_month}/#{new_year_exp}", cc.member, Settings.operation_types.automatic_recycle_credit_card)
+        acc.expire_year = new_year_exp
+      end
     end
     acc
   end
--- app/models/mes_account_updater.rb
+++ app/models/mes_account_updater.rb
@@ -156,15 +156,19 @@ class MesAccountUpdater
         if credit_card.nil?
           Rails.logger.info "CreditCard id not found ##{discretionary_data} while parsing. #{line}"
         else
+          new_expire_year = new_expiration_date[0..1].to_i+2000
+          new_expire_month = new_expiration_date[2..3]
           credit_cards = CreditCard.find_all_by_encrypted_number credit_card.encrypted_number
           credit_cards.each do |cc|
             cc.update_attributes :aus_answered_at => Time.zone.now, :aus_status => response_code if cc.aus_status.nil?
             if cc.active 
               case response_code
               when 'NEWACCT'
-                CreditCard.new_active_credit_card(cc, new_expiration_date[0..1].to_i+2000, new_expiration_date[2..3], new_account_number)
+                # TODO: Call Sebastián credit card update method
+                # CreditCard.new_active_credit_card(cc, new_expiration_date[0..1].to_i+2000, new_expiration_date[2..3], new_account_number)
               when 'NEWEXP'
-                CreditCard.new_active_credit_card(cc, new_expiration_date[0..1].to_i+2000, new_expiration_date[2..3])
+                Auditory.audit(nil, cc, "AUS expiration update from #{cc.expire_month}/#{cc.expire_year} to #{new_expire_month}/#{new_expire_year}", cc.member, Settings.operation_types.aus_recycle_credit_card)
+                cc.update_attributes :expire_year => new_expire_year, :expire_month => new_expire_month
               when 'CLOSED', 'CALL'
                 cc.member.cancel! Time.zone.now, "Automatic cancellation. AUS answered account #{response_code} wont be able to bill"
               else
