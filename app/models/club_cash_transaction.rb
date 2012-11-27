df09aa1b7f8721d55dc77c30618e48d2d528cfe4
sebastian <sebastian@xagax.com>
2012-11-27 14:28:07 -0300

    ref#23810-Moved club cash validation from member to transaction_club_cash model.

--- app/models/club_cash_transaction.rb
+++ app/models/club_cash_transaction.rb
@@ -3,8 +3,10 @@ class ClubCashTransaction < ActiveRecord::Base
 
   attr_accessible :amount, :description
 
-  validates :amount, :presence => true, :numericality => true
+  validates :amount, :presence => true, :numericality => true, :format => {:with => /^(\-)?\d*(.\d{0,2})?$/, :message => "Max amount of digits after comma is 2."}
   
+
+
   def error_to_s(delimiter = ". ")
     self.errors.collect {|attr, message| "#{attr}: #{message}" }.join(delimiter)
   end
