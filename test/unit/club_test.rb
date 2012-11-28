4954b0243ab6f70c0a13c506a3329b331d36bfad
sebastian <sebastian@xagax.com>
2012-11-28 10:03:24 -0300

    ref#23914-Added unit test.

--- test/unit/club_test.rb
+++ test/unit/club_test.rb
@@ -16,9 +16,16 @@ class ClubTest < ActiveSupport::TestCase
   	assert !@club.save, "Club was saved without a partner_id"
   end
   
-  test "After creating a club, it should addtwo product to that club" do
+  test "After creating a club, it should add two product to that club" do
     assert_difference('Product.count',2) do
       @club.save
     end
   end
+
+  test "After creating a club, it should add ten disposition types to that club" do
+    assert_difference('Enumeration.count',13) do  #3 are Member's group type and 10 from disposition types.
+      @club.save
+    end
+  end
+
 end
