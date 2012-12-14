d16dd62742d00cac2b25961293162a9b5332c521
Carla Ares <carla.ares@honou.com.ar>
2012-12-14 11:24:29 -0300

    bug #25354 - model name must be in singular.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,7 +3,7 @@ class TransactionsController < ApplicationController
   before_filter :validate_member_presence
 
   def index
-    my_authorize! :list, Transactions, @current_club.id
+    my_authorize! :list, Transaction, @current_club.id
     respond_to do |format|
       format.html
       format.json { render json: TransactionsDatatable.new(view_context,@current_partner,@current_club,@current_member,@current_agent)}
