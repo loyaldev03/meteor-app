f254cc6e7db0c4811aef1359221980fc8aa1f6a7
Carla Ares <carla.ares@honou.com.ar>
2012-12-14 11:22:20 -0300

    bug #25354 - improved permissions names

--- app/controllers/terms_of_memberships_controller.rb
+++ app/controllers/terms_of_memberships_controller.rb
@@ -4,7 +4,7 @@ class TermsOfMembershipsController < ApplicationController
 
   def show
     @tom = TermsOfMembership.find(params[:id])
-    my_authorize! :manage, TermsOfMembership, @tom.club_id
+    my_authorize! :show, TermsOfMembership, @tom.club_id
     @email_templates = EmailTemplate.find_all_by_terms_of_membership_id(params[:id])
     @payment_gateway_configuration_development = PaymentGatewayConfiguration.find_by_club_id_and_mode(@current_club.id,'development')
     @payment_gateway_configuration_production = PaymentGatewayConfiguration.find_by_club_id_and_mode(@current_club.id,'production')
