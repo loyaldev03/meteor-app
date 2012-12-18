062eb01d6a27340d976af171f4777e2a54d121b0
sebastian <sebastian@xagax.com>
2012-12-18 09:42:29 -0300

    fixed subject of duplicated sync error mails.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -54,6 +54,7 @@ class Notifier < ActionMailer::Base
   def members_with_duplicated_email_sync_error(member_list)
     @members = member_list
     mail :to => Settings.email_to_use_on_duplicated_email_sync_error,
-         :subject => "Members with duplicated email sync error"
+         :subject => "[#{Rails.env}] - #{I18n.l(Time.zone.now, :formta => :only_date )} - Duplicated email sync error."
   end
 end
+ 
