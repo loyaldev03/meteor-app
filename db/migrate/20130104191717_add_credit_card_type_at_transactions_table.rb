9b910d2e7e1eb80452db094fd0dabebaa46ca682
Carla Ares <carla.ares@honou.com.ar>
2013-01-04 18:08:01 -0300

    refs #25462 - forgot to add a migration for transactions table

---
+++ db/migrate/20130104191717_add_credit_card_type_at_transactions_table.rb
@@ -0,0 +1,24 @@
+class AddCreditCardTypeAtTransactionsTable < ActiveRecord::Migration
+  def up
+    add_column :transactions, :cc_type, :string
+    Transaction.all.each do |x|
+      ActiveMerchant::Billing::CreditCard.require_verification_value = false
+      @cc ||= ActiveMerchant::Billing::CreditCard.new(
+        :number     => x.number,
+        :month      => x.expire_month,
+        :year       => x.expire_year,
+        :first_name => x.first_name,
+        :last_name  => x.last_name
+      )
+      @cc.valid?
+      # we will use ActiveMerchant::Billing::CreditCardMethods::CARD_COMPANIES.keys
+      # ["switch", "visa", "diners_club", "master", "forbrugsforeningen", "dankort", 
+      #    "laser", "american_express", "solo", "jcb", "discover", "maestro"]
+      x.cc_type = @cc.type
+      x.save
+    end
+  end
+  def down
+    remove_column :transactions, :cc_type
+  end
+end
