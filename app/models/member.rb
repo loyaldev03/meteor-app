609ed7f55f71a065d36c9c972c972ac2e24c6c64
sebastian <sebastian@xagax.com>
2013-01-08 14:55:06 -0300

    fixed process_sync method on memeber model

--- app/models/member.rb
+++ app/models/member.rb
@@ -857,9 +857,11 @@ class Member < ActiveRecord::Base
       group.each do |member|
         Rails.logger.info "  * processing member ##{member.uuid}"
         api_m = member.api_member
-        if api_m.save!(force: true)
-          unless member.last_sync_error_at
-            Auditory.audit(nil, member, "Member synchronized by batch script", member)
+        unless api_m.nil?
+          if api_m.save!(force: true)
+            unless member.last_sync_error_at
+              Auditory.audit(nil, member, "Member synchronized by batch script", member)
+            end
           end
         end
       end
