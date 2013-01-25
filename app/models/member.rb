46cf0f3b1b5a4a3c07cb3bbc3e2549725d60f312
sebastian <sebastian@xagax.com>
2013-01-25 12:10:40 -0300

    ref#25705-removed unnesseray return

--- app/models/member.rb
+++ app/models/member.rb
@@ -523,7 +523,7 @@ class Member < ActiveRecord::Base
       Airbrake.notify(:error_class => error_message, :error_message => e, :parameters => { :member => self.inspect, :credit_card => credit_card, :enrollment_info => enrollment_info })
       # TODO: this can happend if in the same time a new member is enrolled that makes this an invalid one. Do we have to revert transaction?
       Auditory.audit(agent, self, error_message, self, Settings.operation_types.error_on_enrollment_billing)
-      return { :message => I18n.t('error_messages.member_not_saved', :cs_phone_number => self.club.cs_phone_number), :code => Settings.error_codes.member_not_saved }
+      { :message => I18n.t('error_messages.member_not_saved', :cs_phone_number => self.club.cs_phone_number), :code => Settings.error_codes.member_not_saved }
     end
   end
 
