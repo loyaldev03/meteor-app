02fcf5431bfa3ea84da5f481b5f9d9734cf0961c
sebastian <sebastian@xagax.com>
2013-01-29 13:58:51 -0300

    updated club cash doc

--- app/controllers/api/club_cash_transaction_controller.rb
+++ app/controllers/api/club_cash_transaction_controller.rb
@@ -2,14 +2,13 @@ class Api::ClubCashTransactionController < ApplicationController
 
   # Method : POST
   #
-  # This method adds or remove an specific amount of club cash on a member. In case you want to add club cash, the amount value 
+  # This method adds or deducts an specific amount of club cash on a member. In case you want to add club cash, the amount value 
   # has to be a positive number, while if you want to remove club cash, the amount value has to be negative.  
   #
-  # [url] /api/v1/members/:member_id/club_cash
-  # [member_id] ID of the member. This ID is unique for each member. (32 characters string). This value is used by platform. API users dont know the member id at this moment. 
-  #             Have in mind that this value is part of the url.
-  # [amount] Amount of the club cash to add or It. deduct should be float and only number. We accept a maximun of two digits after the comma. (Example: 2.50 , -10.99, 25)
-  # [description] Description of the club cash. (Eg. why we are adding club cash to the member.)
+  # [url] /api/v1/members/:member_id/club_cash_transaction
+  # [member_id] ID of the member. This ID is unique for each member. (32 characters string). This value is used by platform. Have in mind that this value is part of the url.
+  # [amount] Amount of the club cash to add or deduct. This value should be float and only number. We accept a maximun of two digits after the comma. (Eg: 2.50 , -10.99, 25)
+  # [description] Description of the club cash. (Eg. why we are adding or deducting club cash to the member.)
   # [message] Shows the method results and also informs the errors.
   # [code] Code related to the method result.
   # [errors] A hash with club cash and members errors. This will be use to show errors on club cash cs web page.
