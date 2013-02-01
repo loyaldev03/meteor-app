e322b620b912ace54aef68d0d6345fd73e70ee0a
sebastian <sebastian@xagax.com>
2013-02-01 13:04:23 -0300

    ref#25746-added validation to only let negative stock when allow backorder is true

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,10 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :recurrent, :sku, :stock, :weight, :package, :allow_backorder, :cost_center
 
   validates :sku, :uniqueness => {:scope => :club_id}, :presence => true, :format => /^[a-zA-Z\-_]+$/, :length => { :minimum => 2 }
-  validates :cost_center, :presence => true, :format => /^[a-zA-Z\-_]+$/, :length => { :minimum => 2, :maximum => 19 }
+  validates :cost_center, :format => /^[a-zA-Z\-_]+$/, :length => { :maximum => 19 }, :allow_nil => true
 
   validates :package, :format => /^[a-zA-Z\-_]+$/, :length => { :minimum => 2, :maximum => 30 }
-  validates :stock, :numericality => { :only_integer => true, :less_than => 1999999}
+  validates :stock, :numericality => { :only_integer => true, :less_than => 1999999 }, :allow_backorder => true
 
   before_save :apply_upcase_to_sku
 
---
+++ lib/validators/allow_backorder_validator.rb
@@ -0,0 +1,7 @@
+class AllowBackorderValidator < ActiveModel::EachValidator  
+  def validate(record)
+	  if record.allow_backorder == false and record.stock < 0
+  	  record.errors[:stock] << 'Stock cannot be negative. Enter positive stock, or allow backorder '
+  	end
+  end
+end
