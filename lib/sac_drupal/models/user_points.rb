cf6940c4b82a288fba8a9cb0bc7d2a08ee40205b
Carla Ares <carla.ares@honou.com.ar>
2013-02-08 14:48:37 -0300

    refs #25771 - updating doc with the lastest API method added

---
+++ lib/sac_drupal/models/user_points.rb
@@ -0,0 +1,57 @@
+module Drupal
+  class UserPoints < Struct.new(:member)
+
+    def get
+      res = conn.get('/api/userpoints/%{drupal_id}' % { drupal_id: self.member.api_id }).body
+    rescue Faraday::Error::ParsingError # Drupal sends invalid application/json when something goes wrong
+      Drupal.logger.info "  => #{$!.to_s}"
+    ensure
+      res
+    end
+
+    def create!(options = {})
+      res = conn.post '/api/userpoints/add', fieldmap(options)
+      update_member(res)
+    end
+
+  private
+    def conn
+      self.member.club.drupal
+    end
+
+    def update_member(res)
+      if res
+        data = if res.status == 200
+          { 
+            last_synced_at: Time.now,
+            last_sync_error: nil,
+            last_sync_error_at: nil,
+            sync_status: "synced"
+          }
+        else
+          {
+            last_sync_error: res.body.class == Hash ? res.body[:message] : res.body,
+            last_sync_error_at: Time.now,
+            sync_status: "with_error"
+          }
+        end
+        ::Member.where(uuid: self.member.uuid).limit(1).update_all(data)
+        self.member.reload rescue self.member
+      end
+    end
+
+    def fieldmap(options)
+      {
+        uid: member.api_id,
+        points: options[:amount],
+        operation: "Add",
+        description: options[:description],
+        entity_type: "",
+        entity_id: "" 
+      }
+    end
+  end
+end
+
+
+
