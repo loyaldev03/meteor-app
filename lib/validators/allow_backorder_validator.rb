d4c63296e5e04fdc53ff998e473377394503b894
sebastian <sebastian@xagax.com>
2013-02-13 08:33:29 -0300

    ref#25831-fixed validation.

--- lib/validators/allow_backorder_validator.rb
+++ lib/validators/allow_backorder_validator.rb
@@ -1,7 +1,9 @@
 class AllowBackorderValidator < ActiveModel::EachValidator  
   def validate(record)
-	  if record.allow_backorder == false and record.stock < 0
-  	  record.errors[:stock] << 'Stock cannot be negative. Enter positive stock, or allow backorder '
+  	if not record.stock.nil?
+		  if record.allow_backorder == false and record.stock < 0
+	  	  record.errors[:stock] << 'Stock cannot be negative. Enter positive stock, or allow backorder '
+	  	end
   	end
   end
 end
