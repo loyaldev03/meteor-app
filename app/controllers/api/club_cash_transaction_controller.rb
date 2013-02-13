440e871e0e6244dad61793cba098469e9635a344
sebastian <sebastian@xagax.com>
2013-02-13 09:35:11 -0300

    ref#25734-fixed club cash api doc

--- app/controllers/api/club_cash_transaction_controller.rb
+++ app/controllers/api/club_cash_transaction_controller.rb
@@ -8,8 +8,8 @@ class Api::ClubCashTransactionController < ApplicationController
   # [url] /api/v1/members/:member_id/club_cash_transaction
   # [member_id] ID of the member. This ID is unique for each member. (32 characters string). This value is used by platform. Have in mind that this value is part of the url.
   # [club_cash_transaction] Hash with necessary information to create the  club cash transaction. 
-  #                           *amount: Amount of the club cash to add or deduct. We only accept numbers with up to two digits after the comma. (Eg: 2.50 , -10.99, 25)
-  #                           *description: Description of the club cash. (Eg. why we are adding or deducting club cash to the member.)
+  #                           *amount (required): Amount of the club cash to add or deduct. We only accept numbers with up to two digits after the comma. (Eg: 2.50 , -10.99, 25)
+  #                           *description (optional): Description of the club cash. (Eg. why we are adding or deducting club cash to the member.)
   # [message] Shows the method results and also informs the errors.
   # [code] Code related to the method result.
   # [errors] A hash with club cash and members errors. This will be use to show errors on club cash cs web page.
