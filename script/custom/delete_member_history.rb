fa2dd6ea18b670a1f4940fc72aa74f22ffad1a0a
sebastian <sebastian@xagax.com>
2013-03-13 09:49:45 -0300

    updated delete history script

--- script/custom/delete_member_history.rb
+++ script/custom/delete_member_history.rb
@@ -6,7 +6,7 @@ require 'active_record'
 @log = Logger.new('delet_member.log', 10, 1024000)
 ActiveRecord::Base.logger = @log
 
-ActiveRecord::Base.configurations["phoenix"] = { 
+ActiveRecord::Base.configurations["phoenix"] = {
   :adapter => "mysql2",
   :database => "sac_platform_development",
   :host => "127.0.0.1",
@@ -78,95 +78,61 @@ end
 
 def delete_operations(member)
   tz = Time.now.utc
-  operations = Operation.find_all_by_member_id(member.id)
-  if operations.kind_of?(Array)
-    operations.each do |operation|
-      operation.delete
-    end
-  else
-    operations.delete
-  end
+  member.operations.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member's operations."
 end
 
 def delete_member_notes(member)
   tz = Time.now.utc
-  member_notes = MemberNote.find_all_by_member_id(member.id)
-  member_notes.each do |member_note|
-    member_note.delete
-  end
+  member.member_notes.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member notes."
 end
 
 def delete_member_preferences(member)
   tz = Time.now.utc
-  member_preferences = MemberPreference.find_all_by_member_id(member.id)
-  member_preferences.each do |member_preference|
-    member_preference.delete
-  end
+  member.member_preferences.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member preferences."
 end
 
 def delete_credit_cards(member)
   tz = Time.now.utc
-  credit_cards = CreditCard.find_all_by_member_id(member.id)
-  credit_cards.each do |credit_card|
-    credit_card.delete
-  end
+  member.credit_cards.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member's credit cards."
 end
 
 def delete_transactions(member)
   tz = Time.now.utc
-  transactions = Transaction.find_all_by_member_id(member.id)
-  transactions.each do |transaction|
-    transaction.delete rescue nil
-  end
+  member.transactions.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member's transactions."
 end
 
 def delete_fulfillments(member)
   tz = Time.now.utc
-  fulfillments = Fulfillment.find_all_by_member_id(member.id)
-  fulfillments.each do |fulfillment|
-    fulfillment.delete
-  end
+  member.fulfillments.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member fulfillments."
 end
 
 def delete_communications(member)
   tz = Time.now.utc
-  communications = Communication.find_all_by_member_id(member.id)
-  communications.each do |communication|
-    communication.delete
-  end
+  member.communications.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member communications."
 end
 
 def delete_club_cash_transactions(member)
   tz = Time.now.utc
-  ccts = ClubCashTransaction.find_all_by_member_id(member.id)
-  ccts.each do |cct|
-    cct.delete
-  end
+  member.club_cash_transactions.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member's club cash transactions."
 end
 
 def delete_memberships(member)
   tz = Time.now.utc
-  memberships = Membership.find_all_by_member_id(member.id)
-  memberships.each do |membership|
-    membership.delete
-  end
+  member.memberships.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member memberships."
 end
 
 def delete_enrollment_infos(member)
   tz = Time.now.utc
-  enrollment_infos = EnrollmentInfo.find_all_by_member_id(member.id)
-  enrollment_infos.each do |enrollment_info|
-    enrollment_info.delete
-  end
+  member.enrollment_infos.delete_all
   @log.info "    ... took #{Time.now.utc - tz} to delete member enrollment infos."
 end
 
