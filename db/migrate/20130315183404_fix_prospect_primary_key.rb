f5ac10abbf37453d8866ed1bb87d38ac39051ab6
Carla Ares <carla.ares@honou.com.ar>
2013-03-15 17:20:54 -0300

    commenting all the migration. considering that does not work

--- db/migrate/20130315183404_fix_prospect_primary_key.rb
+++ db/migrate/20130315183404_fix_prospect_primary_key.rb
@@ -1,9 +1,9 @@
 class FixProspectPrimaryKey < ActiveRecord::Migration
   def up
-  	add_index "prospects", ["uuid"], :name => "index_prospects_on_uuid"
+  	# add_index "prospects", ["uuid"], :name => "index_prospects_on_uuid2"
   end
 
   def down
-  	remove_index "prospects", ["uuid"]
+  	# remove_index "prospects", :name => "index_prospects_on_uuid2"
   end
 end
