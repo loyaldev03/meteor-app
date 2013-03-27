b5a06c9d0ac3f0bd829d519c641bc0a9aa2788e7
Carla Ares <carla.ares@honou.com.ar>
2013-03-27 18:22:12 -0300

    refs #26158 - compress all migrations in order to start from scratch. The idea was to have one migration that runs on different databases.

--- db/migrate/20120824192612_add_country_column_on_transactions.rb
+++
@@ -1,9 +0,0 @@
-class AddCountryColumnOnTransactions < ActiveRecord::Migration
-  def up
-  	add_column :transactions, :country, :string 
-  end
-
-  def down
-  	remove_column :transactions, :country
-  end
-end
--- db/migrate/20130122140509_add_allow_back_order_on_product.rb
+++
@@ -1,9 +0,0 @@
-class AddAllowBackOrderOnProduct < ActiveRecord::Migration
-  def up
-  	add_column :products, :allow_backorder, :boolean, :default => false
-  end
-
-  def down
-  	remove_column :products, :allow_backorder
-  end
-end
