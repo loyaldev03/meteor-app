01508e644aa43c98abb754badf12632310fd3acf
sebastian <sebastian@xagax.com>
2013-04-03 11:22:11 -0300

    fixed and improved doc

--- app/controllers/api/operation_controller.rb
+++ app/controllers/api/operation_controller.rb
@@ -8,13 +8,13 @@ class Api::OperationController < ApplicationController
   # @action POST
   #
   # @required [String] api_key Agent's authentication token. This token allows us to check if the agent is allowed to request this action. This token is obtained using token's POST api method. <a href="TokensController.html">TokensMethods</a>
-  # @required [Integer] member_id Member's id. Integer autoincrement value that is used by platform. Have in mind this is part of the url.
+  # @required [Integer] member_id Member's id related to the operation we are creating. Integer autoincrement value that is used by platform. Have in mind this is part of the url.
   # @required [Integer] operation_type message Integer value related to the operation type. Operations supported at the moment:
   #   <ul>
   #     <li><strong>900</strong> vip_event_registration </li>
   #     <li><strong>901</strong> vip_event_cancelation </li>
   #   </ul>
-  # @optional [String] operation_date If this value is nil we save that operation with Time.zone.now. (Format "yyyy-mm-dd")
+  # @optional [String] operation_date Date when the operation was done. If this value is nil we save that operation with Time.zone.now. (Format "yyyy-mm-dd")
   # @optional [Integer] description Description of the operation. It is a text field.
   # @response_field [Integer] code Code related to the method result.
   # @response_field [String] message Shows the method results and also informs the errors.
