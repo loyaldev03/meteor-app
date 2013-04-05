727e0ca87b6432092e089528e291d4ebb377659e
sebastian <sebastian@xagax.com>
2013-04-05 16:29:34 -0300

    added migration and logic related to de ability to disable club_cash within a club

--- app/controllers/api/members_controller.rb
+++ app/controllers/api/members_controller.rb
@@ -476,6 +476,8 @@ class Api::MembersController < ApplicationController
     response = { :message => "This club is not allowed to fix the amount of the club cash on members.", :code => Settings.error_codes.club_cash_cant_be_fixed, :member_id => member.id }
     if params[:amount].blank?
       response = { :message => "Check amount value, please. Amount cannot be blank or null.", :code => Settings.error_codes.wrong_data }
+    elsif not member.club.allow_club_cash_transaction?
+      response = { :message =>I18n.t("error_messages.club_cash_not_supported"), :code => Settings.error_codes.club_does_not_support_club_cash }
     elsif not member.club.club_cash_transactions_enabled
       member.skip_api_sync!
       member.club_cash_amount = params[:amount]
