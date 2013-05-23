15a7415c8cb56e020f6e4046f0c344a1ede126b9
Pablo <pablo.affonso@xagax.com>
2013-05-23 11:15:26 -0300

    Uncommented test

--- test/functional/api/products_controller_test.rb
+++ test/functional/api/products_controller_test.rb
@@ -20,55 +20,55 @@ class Api::ProductsControllerTest < ActionController::TestCase
     assert_equal Settings.error_codes.success, JSON::parse(result.body)['code']
   end
 
-  # test "admin should get stock." do
-  #   sign_in @admin_user
-  #   result = get(:get_stock, { :club_id => @product.club_id, :sku => @product.sku, :format => :json} )
-  #   assert_response :success
-  #   assert_equal Settings.error_codes.success, JSON::parse(result.body)['code']
-  # end
+  test "admin should get stock." do
+    sign_in @admin_user
+    result = get(:get_stock, { :club_id => @product.club_id, :sku => @product.sku, :format => :json} )
+    assert_response :success
+    assert_equal Settings.error_codes.success, JSON::parse(result.body)['code']
+  end
 
-  # test "admin should answer product not found if invalid id is used." do
-  #   sign_in @admin_user
-  #   result = get(:get_stock, { :club_id => @product.club_id, :sku => 'Bracelet34', :format => :json} )
-  #   assert_response :success
-  #   assert_equal Settings.error_codes.not_found, JSON::parse(result.body)['code']
-  # end
+  test "admin should answer product not found if invalid id is used." do
+    sign_in @admin_user
+    result = get(:get_stock, { :club_id => @product.club_id, :sku => 'Bracelet34', :format => :json} )
+    assert_response :success
+    assert_equal Settings.error_codes.not_found, JSON::parse(result.body)['code']
+  end
 
-  # test "api agent should get stock." do
-  #   sign_in @api_user
-  #   result = get(:get_stock, { :club_id => @product.club_id, :sku => @product.sku, :format => :json} )
-  #   assert_response :success
-  #   assert_equal Settings.error_codes.success, JSON::parse(result.body)['code']
-  # end
+  test "api agent should get stock." do
+    sign_in @api_user
+    result = get(:get_stock, { :club_id => @product.club_id, :sku => @product.sku, :format => :json} )
+    assert_response :success
+    assert_equal Settings.error_codes.success, JSON::parse(result.body)['code']
+  end
 
-  # test "api agent should answer product not found if invalid id is used." do
-  #   sign_in @api_user
-  #   result = get(:get_stock, { :club_id => @product.club_id, :sku => 'Bracelet34', :format => :json} )
-  #   assert_response :success
-  #   assert_equal Settings.error_codes.not_found, JSON::parse(result.body)['code']
-  # end
+  test "api agent should answer product not found if invalid id is used." do
+    sign_in @api_user
+    result = get(:get_stock, { :club_id => @product.club_id, :sku => 'Bracelet34', :format => :json} )
+    assert_response :success
+    assert_equal Settings.error_codes.not_found, JSON::parse(result.body)['code']
+  end
 
-  # test "supervisor should not get stock." do
-  #   sign_in @supervisor_user
-  #   result = get(:get_stock, { :club_id => @product.club_id, :sku => @product.sku, :format => :json} )
-  #   assert_response :unauthorized
-  # end
+  test "supervisor should not get stock." do
+    sign_in @supervisor_user
+    result = get(:get_stock, { :club_id => @product.club_id, :sku => @product.sku, :format => :json} )
+    assert_response :unauthorized
+  end
 
-  # test "supervisor should not answer product not found if invalid id is used." do
-  #   sign_in @supervisor_user
-  #   result = get(:get_stock, { :club_id => @product.club_id, :sku => 'Bracelet34', :format => :json} )
-  #   assert_response :unauthorized
-  # end
+  test "supervisor should not answer product not found if invalid id is used." do
+    sign_in @supervisor_user
+    result = get(:get_stock, { :club_id => @product.club_id, :sku => 'Bracelet34', :format => :json} )
+    assert_response :unauthorized
+  end
 
-  # test "representative should not get stock." do
-  #   sign_in @representative_user
-  #   result = get(:get_stock, { :club_id => @product.club_id, :sku => @product.sku, :format => :json} )
-  #   assert_response :unauthorized
-  # end
+  test "representative should not get stock." do
+    sign_in @representative_user
+    result = get(:get_stock, { :club_id => @product.club_id, :sku => @product.sku, :format => :json} )
+    assert_response :unauthorized
+  end
   
-  # test "representative should not answer product not found if invalid id is used." do
-  #   sign_in @representative_user
-  #   result = get(:get_stock, { :club_id => @product.club_id, :sku => 'Bracelet34', :format => :json} )
-  #   assert_response :unauthorized
-  # end
+  test "representative should not answer product not found if invalid id is used." do
+    sign_in @representative_user
+    result = get(:get_stock, { :club_id => @product.club_id, :sku => 'Bracelet34', :format => :json} )
+    assert_response :unauthorized
+  end
 end
