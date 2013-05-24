bb75dc2a178cfd70f46c113caa43b9b1b6e1a528
usuario <sebastian@xagax.com>
2013-05-24 09:29:56 -0300

    removed migration from branch since it not ok here.

--- db/migrate/20130524121300_add_index_on_enrollment_info_on_membership_id.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexOnEnrollmentInfoOnMembershipId < ActiveRecord::Migration
-  def up
-  	execute "ALTER TABLE `enrollment_infos` ADD INDEX `index_membership_id` (`membership_id` ASC);"
-  end
-
-  def down
-  	execute "DROP INDEX index_membership_id ON enrollment_infos"
-  end
-end
