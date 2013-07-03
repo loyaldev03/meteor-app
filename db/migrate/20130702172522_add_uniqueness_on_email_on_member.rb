447ae56fefe95ec6b417fdd9b185ac7d6dc8f538
sebastian <sebastian@xagax.com>
2013-07-03 15:37:52 -0300

    [#51591717]-fixed migration

--- db/migrate/20130702172522_add_uniqueness_on_email_on_member.rb
+++ db/migrate/20130702172522_add_uniqueness_on_email_on_member.rb
@@ -1,11 +1,11 @@
 class AddUniquenessOnEmailOnMember < ActiveRecord::Migration
   def up
   	execute "ALTER TABLE `members` ADD UNIQUE INDEX `email_UNIQUE` (`club_id` ASC, `email` ASC);"
-	execute "ALTER TABLE `members` CHANGE COLUMN `email` `email` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL;"
+	execute "ALTER TABLE `sac_platform_development`.`members` CHANGE COLUMN `email` `email` VARCHAR(255) NOT NULL ;"
   end
 
   def down
   	execute "ALTER TABLE `members` DROP INDEX `email_UNIQUE`;"
-  	execute "ALTER TABLE `members` CHANGE COLUMN `email` `email` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL  "
+  	execute "ALTER TABLE `members` CHANGE COLUMN `email` `email` VARCHAR(255) NULL  "
   end
 end
