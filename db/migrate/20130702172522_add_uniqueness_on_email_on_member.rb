ff110b4683f7e3490798356ae60310fab924f42d
sebastian <sebastian@xagax.com>
2013-07-04 10:48:31 -0300

    fixed migration related to email not null

--- db/migrate/20130702172522_add_uniqueness_on_email_on_member.rb
+++ db/migrate/20130702172522_add_uniqueness_on_email_on_member.rb
@@ -1,7 +1,7 @@
 class AddUniquenessOnEmailOnMember < ActiveRecord::Migration
   def up
   	execute "ALTER TABLE `members` ADD UNIQUE INDEX `email_UNIQUE` (`club_id` ASC, `email` ASC);"
-	execute "ALTER TABLE `sac_platform_development`.`members` CHANGE COLUMN `email` `email` VARCHAR(255) NOT NULL ;"
+	execute "ALTER TABLE `members` CHANGE COLUMN `email` `email` VARCHAR(255) NOT NULL;"
   end
 
   def down
