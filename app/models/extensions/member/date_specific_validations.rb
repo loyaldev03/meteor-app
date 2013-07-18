c5bb1e0b6782d38f531a4ec5b0983320574daa56
sebastian <sebastian@xagax.com>
2013-07-18 10:46:15 -0300

    changed validation method on birh_date

--- app/models/extensions/member/date_specific_validations.rb
+++ app/models/extensions/member/date_specific_validations.rb
@@ -2,9 +2,7 @@ module Extensions
   module Member
     module DateSpecificValidations
       def birth_date_specific_validations!
-        if !self.birth_date.nil? and self.birth_date < '1900-01-01'.to_date
-          
-        end
+        lambda {|member| member.birth_date and member.birth_date < '1900-01-01'.to_date } ? true : false
       end
     end
   end
