e596ccbd8d5f9ba34b9215e5bdc1375f02dbd7ff
Martin Ferrari <martin@xagax.com>
2013-07-24 17:17:45 -0300

    Added routes for TOMs.

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,8 @@ SacPlatform::Application.routes.draw do
       match '/members/new' => 'members#new', as: 'new_member'
       match '/members' => 'members#index', as: 'members', :via => [:get, :post]
       match '/members/search_result' => 'members#search_result', as: 'members_search_result', :via => [:get]
-      match '/terms_of_memberships/:id' => 'terms_of_memberships#show', as: 'terms_of_membership', :via => [:get]
+      
+      resources :terms_of_memberships
 
       scope '/member/:member_prefix' do
         match '/edit' => 'members#edit', as: 'edit_member', :via => [:get]
