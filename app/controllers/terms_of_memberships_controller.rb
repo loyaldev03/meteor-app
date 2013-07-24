e03e2a5d6f4b0e83b01361cbd6677ce69d23208e
Martin Ferrari <martin@xagax.com>
2013-07-24 17:10:08 -0300

    index, new, create and destroy methods added

--- app/controllers/terms_of_memberships_controller.rb
+++ app/controllers/terms_of_memberships_controller.rb
@@ -1,5 +1,32 @@
 class TermsOfMembershipsController < ApplicationController
   before_filter :validate_club_presence
+  # before_filter :check_permissions
+
+  def index
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: TermsOfMembershipDatatable.new(view_context,@current_partner,@current_club,nil,@current_agent) }
+    end
+  end
+
+  def new
+    @tom = TermsOfMembership.new
+  end
+
+  def create
+    @tom = TermsOfMembership.new(params[:tom])
+    @tom.club_id = @current_club.id
+    if @tom.save
+      redirect_to terms_of_membership_path(@current_partner.prefix,@current_club.name, @tom), 
+        notice: 'Your Suscription Plan ' + @tom.name +  'was created Succesfully'
+    else
+      render action: "new"
+    end
+  end
+
+  def destroy
+  end
+
 
   def show
     @tom = TermsOfMembership.find(params[:id])
