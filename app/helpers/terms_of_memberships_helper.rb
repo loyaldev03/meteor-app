86c2a3d2c4bb09142dd1fe473813e5acb63aac22
Martin Ferrari <martin@xagax.com>
2013-08-07 15:22:16 -0300

    Updated helper name for date_spans. Added helper for TOMs select

--- app/helpers/terms_of_memberships_helper.rb
+++ app/helpers/terms_of_memberships_helper.rb
@@ -1,6 +1,12 @@
 module TermsOfMembershipsHelper
-	def select_date_span(select_name)
+	def select_for_date_span(select_name)
 		dates_span = [['Month(s)', 'months'], ['Day(s)', 'days']]
 		select_tag select_name, options_for_select(dates_span), :class => 'input-small'
 	end
+
+	def select_for_toms(select_name)
+		toms = TermsOfMembership.where(:club_id => @current_club.id)
+		select_tag select_name, options_from_collection_for_select(toms, "id", "name"), :include_blank => true
+	end
+
 end
