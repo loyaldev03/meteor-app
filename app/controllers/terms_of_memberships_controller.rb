4c3113ddfc86776691132ed1575d185cfcbbb14b
Martin Ferrari <martin@xagax.com>
2013-08-12 10:43:06 -0300

    Added method to prepare the data to be saved
    Work in progress

--- app/controllers/terms_of_memberships_controller.rb
+++ app/controllers/terms_of_memberships_controller.rb
@@ -14,25 +14,24 @@ class TermsOfMembershipsController < ApplicationController
   end
 
   def create
-    # @tom = TermsOfMembership.new(params[:tom])
+    @tom = TermsOfMembership.new(params[:tom])
     # @tom.club_id = @current_club.id
-    # @tom.agent = @current_agent
-    # if @tom.save
-    #   redirect_to terms_of_membership_path(@current_partner.prefix,@current_club.name, @tom), 
-    #     notice: 'Your Suscription Plan ' + @tom.name +  'was created Succesfully'
-    # else
-    #   render action: "new"
-    # end
+    prepare_tom_data_to_save(params)
+    if @tom.save
+      redirect_to terms_of_memberships_url, notice: "Your Suscription Plan #{@tom.name} (ID: #{@tom.id}) was created Succesfully"
+    else
+      render action: "new"
+    end
   end
 
   def destroy
-    # @tom = TermsOfMembership.find(params[:id])
-    # if @tom.destroy
-    #   redirect_to terms_of_memberships_url, notice: "#{@tom.name} (ID: #{@tom.id}) was successfully destroyed."
-    # else
-    #   flash[:error] = "#{@tom.name} (ID: #{@tom.id}) was not destroyed."
-    #   redirect_to terms_of_memberships_url
-    # end
+    @tom = TermsOfMembership.find(params[:id])
+    if @tom.destroy
+      redirect_to terms_of_memberships_url, notice: "Suscription Plan #{@tom.name} (ID: #{@tom.id}) was successfully destroyed."
+    else
+      flash[:error] = "Suscription Plan #{@tom.name} (ID: #{@tom.id}) was not destroyed."
+      redirect_to terms_of_memberships_url
+    end
   end
 
   def show
@@ -43,4 +42,45 @@ class TermsOfMembershipsController < ApplicationController
     @payment_gateway_configuration_production = PaymentGatewayConfiguration.find_by_club_id_and_mode(@current_club.id,'production')
   end
 
+  private
+    def prepare_tom_data_to_save(post_data)
+      @tom.club_id = @current_club.id
+      @tom.agent_id = @current_agent.id
+      
+      # Step #1
+      @tom.name = post_data[:terms_of_membership][:name]
+      @tom.api_role = post_data[:terms_of_membership][:api_role]
+      @tom.description = post_data[:terms_of_membership][:description]
+
+      # Step 2
+      @tom.initial_fee = post_data[:initial_fee]
+      @tom.trial_period_amount = post_data[:trial_period_amount]
+      @tom.provisional_days = post_data[:trial_period_lasting_time_span] == 'months' ? months_to_days(post_data[:trial_period_lasting]) : post_data[:trial_period_lasting]
+      @tom.is_payment_expected = post_data[:is_payment_expected] == 'yes' ? 1 : 0
+      @tom.installment_amount = post_data[:installment_amount]
+      @tom.installment_period = post_data[:installment_amount_days_time_span] == 'months' ? months_to_days(post_data[:installment_amount_days]) : post_data[:installment_amount_days]
+      @tom.suscription_terms = 
+        if post_data[:suscription_terms] == 'until_cancelled'
+          0
+        else
+          post_data[:suscription_terms_stop_billing_after_time_span] == 'months' ? 
+            months_to_days(post_data[:suscription_terms_stop_billing_after]) : post_data[:suscription_terms_stop_billing_after]
+        end
+      
+      # Step 3
+
+      
+    end
+
+    def months_to_days(months)
+      if months > 0
+        if months % 12 == 0
+          months / 12 * 365
+        else
+          months * 30
+        end
+      else
+        0
+      end
+    end
 end
