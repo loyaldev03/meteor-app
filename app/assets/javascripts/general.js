00b047ad6a430a915129d39a5b37cd88460a7ef3
Martin Ferrari <martin@xagax.com>
2013-08-13 18:01:56 -0300

    Removed legacy and test code

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -884,12 +884,6 @@ function tom_create_wizard() {
     focusFirstInput : true,
     disableUIStyles: true,
     textNext: '',
-    textSubmit: '',
-    formOptions :{
-      success: function(data){$("#status").fadeTo(500,1,function(){ $(this).html("You are now registered!").fadeTo(5000, 0); })},
-      beforeSubmit: function(data){$("#data").html("data sent to the server: " + $.param(data));},
-      dataType: 'json',
-      resetForm: true
-    } 
+    textSubmit: ''
   });
 }
