be7e54a7f6cb34753d40740b9f08e7ab1304f34a
Martin Ferrari <martin@xagax.com>
2013-08-13 14:45:00 -0300

    Test cases added
    Not ready yet

--- test/integration/terms_of_membership_test.rb
+++ test/integration/terms_of_membership_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TermsOfMembershipTests < ActionController::IntegrationTest
 
-  setup do
+	setup do
 		init_test_setup
 		@admin_agent = FactoryGirl.create(:confirmed_admin_agent)
 		@partner = FactoryGirl.create(:partner)
@@ -10,40 +10,155 @@ class TermsOfMembershipTests < ActionController::IntegrationTest
 		sign_in_as(@admin_agent)
 	end
 
-	test "Delete unused tom" do
-		the_tom = FactoryGirl.create(:terms_of_membership_with_gateway, :club_id => @club.id)
+	# test "Delete unused TOM" do
+	# 	13.times { the_tom = FactoryGirl.create(:terms_of_membership_with_gateway, :club_id => @club.id) }
+	# 	the_tom = TermsOfMembership.last
+	# 	visit terms_of_memberships_path(@partner.prefix, @club.name)
+	# 	within('#terms_of_memberships_table') do
+	# 		find('.sorting_asc', :text => 'ID').click # Sorting desc to show the last tom we had created as the first row of the table
+	# 		within("tr", :text => the_tom.id.to_s) do
+	# 			confirm_ok_js
+	# 			click_link_or_button "Destroy"
+	# 		end
+	# 	end
+	# 	assert page.has_content?("was successfully destroyed.")
+	# end
+
+	# test "Do not delete a TOM with inactive memberships" do
+	# 	27.times { the_tom = FactoryGirl.create(:terms_of_membership_with_gateway, :club_id => @club.id) }
+	# 	the_tom = TermsOfMembership.last
+	# 	the_lapsed_member = create_active_member(the_tom, :lapsed_member, nil, {}, { :created_by => @admin_agent })
+	# 	visit terms_of_memberships_path(@partner.prefix, @club.name)
+	# 	within('#terms_of_memberships_table') do
+	# 		find('.sorting_asc', :text => 'ID').click # Sorting desc to show the last tom we had created as the first row of the table
+	# 		within("tr", :text => the_tom.id.to_s) do
+	# 			confirm_ok_js
+	# 			click_link_or_button "Destroy"
+	# 		end
+	# 	end
+	# 	assert page.has_content?("was not destroyed.")
+	# end
+
+	# test "Do not delete a TOM with active memberships" do
+	# 	27.times { the_tom = FactoryGirl.create(:terms_of_membership_with_gateway, :club_id => @club.id) }
+	# 	the_tom = TermsOfMembership.last
+	# 	the_active_member = create_active_member(the_tom, :active_member, nil, {}, { :created_by => @admin_agent })
+	# 	visit terms_of_memberships_path(@partner.prefix, @club.name)
+	# 	within('#terms_of_memberships_table') do
+	# 		find('.sorting_asc', :text => 'ID').click # Sorting desc to show the last tom we had created as the first row of the table
+	# 		within("tr", :text => the_tom.id.to_s) do
+	# 			confirm_ok_js
+	# 			click_link_or_button "Destroy"
+	# 		end
+	# 	end
+	# 	assert page.has_content?("was not destroyed.")
+	# end
+
+	# test "Create subcription plan with Initial Fee distinct of 0" do
+	# 	tom_name = 'TOM Initial Fee greater than 0'
+	# 	visit terms_of_memberships_path(@partner.prefix, @club.name)
+	# 	click_link_or_button 'Add New Plan'
+	# 	fill_in 'terms_of_membership_name', :with => tom_name
+	# 	click_link_or_button 'Membership Terms'
+	# 	fill_in 'initial_fee_amount', :with => '1'
+	# 	fill_in 'trial_period_amount', :with => '0'
+	# 	fill_in 'trial_period_lasting', :with => '0'
+	# 	fill_in 'installment_amount', :with => '0'
+	# 	fill_in 'installment_amount_days', :with => '0'
+	# 	click_link_or_button 'Define Upgrades / Downgrades'
+	# 	click_link_or_button 'Create Plan'
+	# 	assert page.has_content?('was created Succesfully') # TOM was created
+	# 	assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
+	# end
+
+	# test "Create subcription plan with Initial Fee equal to 0" do
+	# 	tom_name = 'TOM Initial Fee equal to 0'
+	# 	visit terms_of_memberships_path(@partner.prefix, @club.name)
+	# 	click_link_or_button 'Add New Plan'
+	# 	fill_in 'terms_of_membership_name', :with => tom_name
+	# 	click_link_or_button 'Membership Terms'
+	# 	fill_in 'initial_fee_amount', :with => '0'
+	# 	fill_in 'trial_period_amount', :with => '0'
+	# 	fill_in 'trial_period_lasting', :with => '0'
+	# 	fill_in 'installment_amount', :with => '0'
+	# 	fill_in 'installment_amount_days', :with => '0'
+	# 	click_link_or_button 'Define Upgrades / Downgrades'
+	# 	click_link_or_button 'Create Plan'
+	# 	assert page.has_content?('was created Succesfully') # TOM was created
+	# 	assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
+	# end
+
+
+	test "Create subcription plan with Free Trial Period in Days" do
+		tom_name = 'TOM Trial Period in Days'
 		visit terms_of_memberships_path(@partner.prefix, @club.name)
-		within('#terms_of_memberships_table') do
-			click_link_or_button 'Destroy'
-		end
-		page.driver.browser.switch_to.alert.accept
-		assert page.has_content?("was successfully destroyed.")
+		click_link_or_button 'Add New Plan'
+		fill_in 'terms_of_membership_name', :with => tom_name
+		click_link_or_button 'Membership Terms'
+		fill_in 'initial_fee_amount', :with => '0'
+		fill_in 'trial_period_amount', :with => '0'
+		fill_in 'trial_period_lasting', :with => '5'
+		select('Day(s)', :from => 'trial_period_lasting_time_span')
+		fill_in 'installment_amount', :with => '0'
+		fill_in 'installment_amount_days', :with => '0'
+		click_link_or_button 'Define Upgrades / Downgrades'
+		click_link_or_button 'Create Plan'
+		assert page.has_content?('was created Succesfully') # TOM was created
+		assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
 	end
 
-	test "Do not delete a TOM with inactive memberships" do
-		the_tom = FactoryGirl.create(:terms_of_membership_with_gateway, :club_id => @club.id)
-		the_lapsed_member = FactoryGirl.create(:lapsed_member, :club_id => @club.id)
-		the_membership = FactoryGirl.create(:lapsed_member_membership, :member_id => the_lapsed_member.id, :terms_of_membership_id => the_tom.id)
-
+	test "Create subcription plan with Free Trial Period in Months" do
+		tom_name = 'TOM Trial Period in Months'
 		visit terms_of_memberships_path(@partner.prefix, @club.name)
-		within('#terms_of_memberships_table') do
-			click_link_or_button 'Destroy'
-		end
-		page.driver.browser.switch_to.alert.accept
-		assert page.has_content?("was not destroyed.")
+		click_link_or_button 'Add New Plan'
+		fill_in 'terms_of_membership_name', :with => tom_name
+		click_link_or_button 'Membership Terms'
+		fill_in 'initial_fee_amount', :with => '0'
+		fill_in 'trial_period_amount', :with => '0'
+		fill_in 'trial_period_lasting', :with => '5'
+		select('Month(s)', :from => 'trial_period_lasting_time_span')
+		fill_in 'installment_amount', :with => '0'
+		fill_in 'installment_amount_days', :with => '0'
+		click_link_or_button 'Define Upgrades / Downgrades'
+		click_link_or_button 'Create Plan'
+		assert page.has_content?('was created Succesfully') # TOM was created
+		assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
 	end
 
-	test "Do not delete a TOM with active memberships" do
-		the_tom = FactoryGirl.create(:terms_of_membership_with_gateway, :club_id => @club.id)
-		the_active_member = FactoryGirl.create(:active_member, :club_id => @club.id)
-		the_membership = FactoryGirl.create(:applied_member_membership, :member_id => the_active_member.id, :terms_of_membership_id => the_tom.id)
-
+	test "Create subcription plan with Paid Trial Period in Days" do
+		tom_name = 'TOM with Paid Trial Period in Days'
 		visit terms_of_memberships_path(@partner.prefix, @club.name)
-		within('#terms_of_memberships_table') do
-			click_link_or_button 'Destroy'
-		end
-		page.driver.browser.switch_to.alert.accept
-		assert page.has_content?("was not destroyed.")
+		click_link_or_button 'Add New Plan'
+		fill_in 'terms_of_membership_name', :with => tom_name
+		click_link_or_button 'Membership Terms'
+		fill_in 'initial_fee_amount', :with => '0'
+		fill_in 'trial_period_amount', :with => '100'
+		fill_in 'trial_period_lasting', :with => '5'
+		select('Day(s)', :from => 'trial_period_lasting_time_span')
+		fill_in 'installment_amount', :with => '0'
+		fill_in 'installment_amount_days', :with => '0'
+		click_link_or_button 'Define Upgrades / Downgrades'
+		click_link_or_button 'Create Plan'
+		assert page.has_content?('was created Succesfully') # TOM was created
+		assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
+	end
+
+	test " Create subcription plan with Paid Trial Period in Months" do
+		tom_name = 'TOM with Paid Trial Period in Months'
+		visit terms_of_memberships_path(@partner.prefix, @club.name)
+		click_link_or_button 'Add New Plan'
+		fill_in 'terms_of_membership_name', :with => tom_name
+		click_link_or_button 'Membership Terms'
+		fill_in 'initial_fee_amount', :with => '0'
+		fill_in 'trial_period_amount', :with => '100'
+		fill_in 'trial_period_lasting', :with => '5'
+		select('Month(s)', :from => 'trial_period_lasting_time_span')
+		fill_in 'installment_amount', :with => '0'
+		fill_in 'installment_amount_days', :with => '0'
+		click_link_or_button 'Define Upgrades / Downgrades'
+		click_link_or_button 'Create Plan'
+		assert page.has_content?('was created Succesfully') # TOM was created
+		assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
 	end
 
 end
