38a0da4ded92bb80296fe1a18219ce590d43b5bd
Martin Ferrari <martin@xagax.com>
2013-08-13 22:20:42 -0300

    Update payment_is_expected values. Convert days to integer

--- app/controllers/terms_of_memberships_controller.rb
+++ app/controllers/terms_of_memberships_controller.rb
@@ -45,20 +45,18 @@ class TermsOfMembershipsController < ApplicationController
     def prepare_tom_data_to_save(post_data)
       @tom.club_id = @current_club.id
       @tom.agent_id = @current_agent.id
-      
       # Step #1
       @tom.name = post_data[:terms_of_membership][:name]
       @tom.api_role = post_data[:terms_of_membership][:api_role]
       @tom.description = post_data[:terms_of_membership][:description]
-
       # Step 2
       @tom.initial_fee = post_data[:initial_fee_amount]
       @tom.trial_period_amount = post_data[:trial_period_amount]
-      @tom.provisional_days = post_data[:trial_period_lasting_time_span] == 'months' ? months_to_days(post_data[:trial_period_lasting].to_i) : post_data[:trial_period_lasting]
-      @tom.is_payment_expected = post_data[:is_payment_expected] == 'yes'
-      if @tom.is_payment_expected
+      @tom.provisional_days = post_data[:trial_period_lasting_time_span] == 'months' ? months_to_days(post_data[:trial_period_lasting].to_i) : post_data[:trial_period_lasting].to_i
+      @tom.is_payment_expected = post_data[:is_payment_expected] == 'yes' ? 1 : 0
+      if @tom.is_payment_expected == 1
         @tom.installment_amount = post_data[:installment_amount]
-        @tom.installment_period = post_data[:installment_amount_days_time_span] == 'months' ? months_to_days(post_data[:installment_amount_days].to_i) : post_data[:installment_amount_days]
+        @tom.installment_period = post_data[:installment_amount_days_time_span] == 'months' ? months_to_days(post_data[:installment_amount_days].to_i) : post_data[:installment_amount_days].to_i
       else
         @tom.installment_amount = nil
         @tom.installment_period = nil
@@ -67,23 +65,22 @@ class TermsOfMembershipsController < ApplicationController
         if post_data[:suscription_terms] == 'until_cancelled'
           0
         else
-          post_data[:suscription_terms_stop_billing_after_time_span] == 'months' ? months_to_days(post_data[:suscription_terms_stop_billing_after].to_i) : post_data[:suscription_terms_stop_billing_after]
+          post_data[:suscription_terms_stop_billing_after_time_span] == 'months' ? months_to_days(post_data[:suscription_terms_stop_billing_after].to_i) : post_data[:suscription_terms_stop_billing_after].to_i
         end
-      
       # Step 3
       case post_data[:if_cannot_bill_member]
       when 'cancel'
         @tom.if_cannot_bill = 'cancel'
       when 'suspend'
         @tom.if_cannot_bill = 'suspend'
-        @tom.suspension_period = post_data[:if_cannot_bill_member_suspend_for_time_span] == 'months' ? months_to_days(post_data[:if_cannot_bill_member_suspend_for].to_i) : post_data[:if_cannot_bill_member_suspend_for]
+        @tom.suspension_period = post_data[:if_cannot_bill_member_suspend_for_time_span] == 'months' ? months_to_days(post_data[:if_cannot_bill_member_suspend_for].to_i) : post_data[:if_cannot_bill_member_suspend_for].to_i
       when 'downgrade_to'
         @tom.if_cannot_bill = 'downgrade_tom'
-        @tom.downgrade_tom_id = post_data[:downgrade_to_tom].to_i
+        @tom.downgrade_tom_id = post_data[:downgrade_to_tom]
       end
       if post_data[:upgrade_to_tom] != '' and post_data[:upgrade_to_tom_days].to_i > 0
         @tom.upgrade_tom_id = post_data[:upgrade_to_tom]
-        @tom.upgrade_tom_period = post_data[:upgrade_to_tom_days_time_span] == 'months' ? months_to_days(post_data[:upgrade_to_tom_days].to_i) : post_data[:upgrade_to_tom_days]
+        @tom.upgrade_tom_period = post_data[:upgrade_to_tom_days_time_span] == 'months' ? months_to_days(post_data[:upgrade_to_tom_days].to_i) : post_data[:upgrade_to_tom_days].to_i
       end
     end
 
