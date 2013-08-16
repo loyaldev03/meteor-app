fe3da6c880e435f01d264c39de2da57f2b87667e
Martin Ferrari <martin@xagax.com>
2013-08-16 16:45:56 -0300

    Rearranged options in select. Added optional selected_item parameter

--- app/helpers/terms_of_memberships_helper.rb
+++ app/helpers/terms_of_memberships_helper.rb
@@ -1,10 +1,10 @@
 module TermsOfMembershipsHelper
-	def select_for_date_span(select_name)
-		dates_span = [['Month(s)', 'months'], ['Day(s)', 'days']]
+	def select_for_date_span(select_name, selected_iten = nil)
+		dates_span = [['Day(s)', 'days'], ['Month(s)', 'months']]
 		select_tag select_name, options_for_select(dates_span), :class => 'input-small'
 	end
 
-	def select_for_toms(select_name)
+	def select_for_toms(select_name, selected_iten = nil)
 		toms = TermsOfMembership.where(:club_id => @current_club.id)
 		select_tag select_name, options_from_collection_for_select(toms, "id", "name"), :include_blank => true
 	end
