5664c288494d582d99bbdfc642632f178cfd2788
sebastian <sebastian@xagax.com>
2013-08-28 11:06:44 -0300

    removed wrong migration related to installment period being set to decimal

--- db/migrate/20130827135556_change_installment_period_to_decimal.rb
+++
@@ -1,9 +0,0 @@
-class ChangeInstallmentPeriodToDecimal < ActiveRecord::Migration
-  def up
-  	change_column :terms_of_memberships, :installment_period, :decimal, :precision => 11, :scale => 2
-  end
-
-  def down
-  	change_column :terms_of_memberships, :installment_period, :integer
-  end
-end
