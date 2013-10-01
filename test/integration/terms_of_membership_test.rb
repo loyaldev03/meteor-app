8354b62d72a8f3b8ade0600870616889877793c8
sebastian <sebastian@xagax.com>
2013-10-01 16:16:01 -0300

    [#55166650] removed unnused fields from terms of membership mannager.

--- test/integration/terms_of_membership_test.rb
+++ test/integration/terms_of_membership_test.rb
@@ -172,22 +172,22 @@ class TermsOfMembershipTests < ActionController::IntegrationTest
 		assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
 	end
 
-	test "Create subcription plan with No payment is expected" do
-		tom_name = 'TOM with No payment expected'
-		visit terms_of_memberships_path(@partner.prefix, @club.name)
-		click_link_or_button 'Add New Plan'
+	# test "Create subcription plan with No payment is expected" do
+	# 	tom_name = 'TOM with No payment expected'
+	# 	visit terms_of_memberships_path(@partner.prefix, @club.name)
+	# 	click_link_or_button 'Add New Plan'
 
-		fill_in_step_1(tom_name)
-		click_link_or_button 'Define Membership Terms'
-		fill_in_step_2({initial_fee_amount:0, trial_period_amount:1, trial_period_lasting:0},{},["is_payment_expected_no"])
-		click_link_or_button 'Define Upgrades / Downgrades'
+	# 	fill_in_step_1(tom_name)
+	# 	click_link_or_button 'Define Membership Terms'
+	# 	fill_in_step_2({initial_fee_amount:0, trial_period_amount:1, trial_period_lasting:0},{},["is_payment_expected_no"])
+	# 	click_link_or_button 'Define Upgrades / Downgrades'
 
-		find("label", :text => "If we cannot bill a member then")
-		choose('if_cannot_bill_member_cancel')
-		click_link_or_button 'Create Plan'
-		assert page.has_content?('was created succesfully') # TOM was created
-		assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
-	end
+	# 	find("label", :text => "If we cannot bill a member then")
+	# 	choose('if_cannot_bill_member_cancel')
+	# 	click_link_or_button 'Create Plan'
+	# 	assert page.has_content?('was created succesfully') # TOM was created
+	# 	assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
+	# end
 
 	test "Create subcription plan with Stop billing after at Subscription Terms - month" do
 		tom_name = 'TOM with with Stop billing after Months'
@@ -518,28 +518,28 @@ class TermsOfMembershipTests < ActionController::IntegrationTest
 		assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
 	end
 
-	test "Update subcription plan with No payment is expected - No membership associated" do
-		tom_name = 'TOM Name'
-		tom = FactoryGirl.create(:terms_of_membership_with_gateway, :club_id => @club.id, :name => tom_name)
-		visit terms_of_memberships_path(@partner.prefix, @club.name)
-		within('#terms_of_memberships_table') do
-			find('.sorting_asc', :text => 'ID').click # Sorting desc to show the last tom we had created as the first row of the table
-			within("tr", :text => tom_name) do
-				click_link_or_button "Edit"
-			end
-		end
+	# test "Update subcription plan with No payment is expected - No membership associated" do
+	# 	tom_name = 'TOM Name'
+	# 	tom = FactoryGirl.create(:terms_of_membership_with_gateway, :club_id => @club.id, :name => tom_name)
+	# 	visit terms_of_memberships_path(@partner.prefix, @club.name)
+	# 	within('#terms_of_memberships_table') do
+	# 		find('.sorting_asc', :text => 'ID').click # Sorting desc to show the last tom we had created as the first row of the table
+	# 		within("tr", :text => tom_name) do
+	# 			click_link_or_button "Edit"
+	# 		end
+	# 	end
 
-		fill_in_step_1(tom_name + ' Updated')
-		click_link_or_button 'Edit Membership Terms'
-		fill_in_step_2({initial_fee_amount:10, trial_period_amount:20, trial_period_lasting:30},{trial_period_lasting_time_span:"Month(s)"},["is_payment_expected_no","subscription_terms_until_cancelled"])
-		click_link_or_button 'Edit Upgrades / Downgrades'
+	# 	fill_in_step_1(tom_name + ' Updated')
+	# 	click_link_or_button 'Edit Membership Terms'
+	# 	fill_in_step_2({initial_fee_amount:10, trial_period_amount:20, trial_period_lasting:30},{trial_period_lasting_time_span:"Month(s)"},["is_payment_expected_no","subscription_terms_until_cancelled"])
+	# 	click_link_or_button 'Edit Upgrades / Downgrades'
 		
-		find("label", :text => "If we cannot bill a member then")
-		choose('if_cannot_bill_member_cancel')
-		click_link_or_button 'Update Plan'
-		assert page.has_content?('was updated succesfully') # TOM was created
-		assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
-	end
+	# 	find("label", :text => "If we cannot bill a member then")
+	# 	choose('if_cannot_bill_member_cancel')
+	# 	click_link_or_button 'Update Plan'
+	# 	assert page.has_content?('was updated succesfully') # TOM was created
+	# 	assert page.find('#terms_of_memberships_table').has_content?(tom_name) # TOM is in the table
+	# end
 
 	test "Update subcription plan with Stop billing after at Subscription Terms - month  - No membership associated" do
 		tom_name = 'TOM Name'
