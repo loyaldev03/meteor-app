6ed75a8ee869c0bd726ea5a2ce9f1527c0e19da1
sebastian <sebastian@xagax.com>
2013-10-02 15:30:21 -0300

    [#55166650] removed unnecesary fields from tom manager form

--- app/models/terms_of_membership.rb
+++ app/models/terms_of_membership.rb
@@ -26,7 +26,7 @@ class TermsOfMembership < ActiveRecord::Base
   validates :installment_type, :presence => true
   validates :initial_club_cash_amount, :numericality => { :greater_than_or_equal_to => 0 }
   validates :club_cash_installment_amount, :numericality => { :greater_than_or_equal_to => 0 }
-  validates :initial_fee, :numericality => { :greater_than_or_equal_to => 0 }
+  # validates :initial_fee, :numericality => { :greater_than_or_equal_to => 0 }
   validates :trial_period_amount, :numericality => { :greater_than_or_equal_to => 0 }
   validates :is_payment_expected, :presence => true
   validates :subscription_limits, :numericality => { :greater_than_or_equal_to => 0 }
