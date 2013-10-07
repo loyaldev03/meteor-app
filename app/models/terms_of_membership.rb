e2af7a503f1da9b10386c3b4aa2890e5d9449248
sebastian <sebastian@xagax.com>
2013-10-07 09:31:55 -0300

    [#58156882] added validation on tom's installment_amount

--- app/models/terms_of_membership.rb
+++ app/models/terms_of_membership.rb
@@ -22,7 +22,7 @@ class TermsOfMembership < ActiveRecord::Base
   #validates :needs_enrollment_approval, :presence => true
   validates :club, :presence => true
   validates :provisional_days, :presence => true
-  # validates :installment_amount, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :installment_amount, :numericality => { :greater_than_or_equal_to => 0 }
   # validates :installment_period, :numericality => { :greater_than_or_equal_to => 0 }
   validates :installment_type, :presence => true
   validates :initial_club_cash_amount, :numericality => { :greater_than_or_equal_to => 0 }
