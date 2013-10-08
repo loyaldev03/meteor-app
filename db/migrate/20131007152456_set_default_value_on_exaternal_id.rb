3fb9e5c2c5d13586a91260a2a826e2a3d11283f9
Carla Ares <carla.ares@gmail.com>
2013-10-08 15:17:59 -0300

    [#52192829] reverting migrations

--- db/migrate/20131007152456_set_default_value_on_exaternal_id.rb
+++
@@ -1,9 +0,0 @@
-class SetDefaultValueOnExaternalId < ActiveRecord::Migration
-  def up
-    change_column :members, :external_id, :string, :default => ''
-  end
-
-  def down
-    change_column :members, :external_id, :string, :default => nil
-  end
-end
