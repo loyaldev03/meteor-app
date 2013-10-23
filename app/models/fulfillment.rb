1bbba7b13878cca9d75c36f6bccbda0b6622cf89
sebastian <sebastian@xagax.com>
2013-10-23 17:51:43 -0300

    fixed syntax on commented line

--- app/models/fulfillment.rb
+++ app/models/fulfillment.rb
@@ -1,7 +1,7 @@
 #
 # assigned_at => The day this fulfillment is assigned to our member.
 #     If fulfillment is resend it is because of a wrong address. 
-#     this value will be 3d.
+#     this value will be updated.
 # renewable_at => This day is assigned_at + 1.year (at first time). 
 #     Will be used by fulfillment script to check which members need a new fulfillment
 # status => open , archived
