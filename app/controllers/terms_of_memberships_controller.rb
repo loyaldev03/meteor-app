00118bc843f7d2812d329d4c81f992da8dc8846e
sebastian <delucasd@gmail.com>
2013-11-11 18:54:39 -0200

    [#60545228] fixed tom's show page. Removed certain relations with mode, which was removed.

--- app/controllers/terms_of_memberships_controller.rb
+++ app/controllers/terms_of_memberships_controller.rb
@@ -63,8 +63,7 @@ class TermsOfMembershipsController < ApplicationController
     @tom = TermsOfMembership.find(params[:id])
     my_authorize! :show, TermsOfMembership, @tom.club_id
     @email_templates = EmailTemplate.find_all_by_terms_of_membership_id(params[:id])
-    @payment_gateway_configuration_development = PaymentGatewayConfiguration.find_by_club_id_and_mode(@current_club.id,'development')
-    @payment_gateway_configuration_production = PaymentGatewayConfiguration.find_by_club_id_and_mode(@current_club.id,'production')
+    @payment_gateway_configuration = @tom.club.payment_gateway_configuration
   end
 
   private
