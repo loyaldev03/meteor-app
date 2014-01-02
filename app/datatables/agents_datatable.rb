c84dd27621562e8c96188af46616ab816b22078f
sebastian <delucasd@gmail.com>
2014-01-02 15:32:44 -0200

    [#62058972] fixed datatable buttons. If one is not shown, it should not throw nill error

--- app/datatables/agents_datatable.rb
+++ app/datatables/agents_datatable.rb
@@ -18,11 +18,11 @@ private
         agent.email, 
         agent.username,
         I18n.l(agent.created_at,:format=>:long),
-        (link_to(I18n.t(:show), @url_helpers.admin_agent_path(agent), :class => 'btn btn-mini') if @current_agent.can? :read, Agent)+
-        (link_to(I18n.t(:edit), @url_helpers.edit_admin_agent_path(agent), :class => 'btn btn-mini') if @current_agent.can? :edit, Agent)+
+        (link_to(I18n.t(:show), @url_helpers.admin_agent_path(agent), :class => 'btn btn-mini') if @current_agent.can? :read, Agent).to_s+
+        (link_to(I18n.t(:edit), @url_helpers.edit_admin_agent_path(agent), :class => 'btn btn-mini') if @current_agent.can? :edit, Agent).to_s+
         (link_to(I18n.t(:destroy), @url_helpers.admin_agent_path(agent), :method => :delete,
                         :confirm => I18n.t("are_you_sure"),
-                        :class => 'btn btn-mini btn-danger')if @current_agent.can? :delete, Agent)
+                        :class => 'btn btn-mini btn-danger')if @current_agent.can? :delete, Agent).to_s
       ]
     end
   end
