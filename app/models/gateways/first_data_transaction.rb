72002df9379c95cb66f0c87e859fe5a619cb314c
sebastian <delucasd@gmail.com>
2014-04-23 11:04:33 -0300

    [#68230566] added FirstData gateway

---
+++ app/models/gateways/first_data_transaction.rb
@@ -0,0 +1,42 @@
+class FirstDataTransaction < Transaction
+
+  def member=(member)
+    super(member)
+    self.invoice_number = member.id
+  end
+
+  def self.store!(am_credit_card, pgc)
+  	ActiveMerchant::Billing::Base.mode = ( Rails.env.production? ? :production : :test )
+    login_data = { :login => pgc.login, :password => pgc.password }
+    gateway = ActiveMerchant::Billing::FirstdataE4Gateway.new(login_data)
+    gateway.store(am_credit_card).authorization
+  end
+
+  def fill_transaction_type_for_credit(sale_transaction)
+    self.transaction_type = "refund"
+    self.refund_response_transaction_id = sale_transaction.response_transaction_id
+  end
+  
+  private
+
+    def credit_card_token
+      self.token
+    end
+
+    def save_response(answer)
+      self.response = answer
+      self.success = answer.success?
+      if answer.params
+        self.response_auth_code = answer.params['authorization_num']
+      end
+      self.response_transaction_id = answer.authorization
+      self.response_result = answer.message
+      save
+      super(answer)
+    end
+
+    def load_gateway(recurrent = false)
+      login_data = { :login => login, :password => password }
+      @gateway = ActiveMerchant::Billing::FirstdataE4Gateway.new(login_data)
+    end
+end
