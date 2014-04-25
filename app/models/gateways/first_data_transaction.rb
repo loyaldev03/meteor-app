4ac9434deb1ce2d4ca909e6e22a0db80d2bb2663
sebastian <delucasd@gmail.com>
2014-04-25 11:13:58 -0300

    [#68230566] fixed first data logic.

--- app/models/gateways/first_data_transaction.rb
+++ app/models/gateways/first_data_transaction.rb
@@ -9,18 +9,23 @@ class FirstDataTransaction < Transaction
   	ActiveMerchant::Billing::Base.mode = ( Rails.env.production? ? :production : :test )
     login_data = { :login => pgc.login, :password => pgc.password }
     gateway = ActiveMerchant::Billing::FirstdataE4Gateway.new(login_data)
-    gateway.store(am_credit_card).authorization
+    answer = gateway.store(am_credit_card)
+    raise answer.message unless answer.success?    
+    answer.params["transarmor_token"] if answer.params
+  rescue Exception => e
+    logger.error "AM::Store::Answer => " + answer.inspect
+    raise e
   end
 
   def fill_transaction_type_for_credit(sale_transaction)
     self.transaction_type = "refund"
-    self.refund_response_transaction_id = sale_transaction.response_transaction_id
+    self.refund_response_transaction_id = [sale_transaction.response_auth_code, sale_transaction.response_transaction_id, sale_transaction.amount.to_s.gsub(".","")].join(";")
   end
   
   private
 
     def credit_card_token
-      self.token
+      [self.token, self.cc_type, self.member.first_name, self.member.last_name, self.expire_month, self.expire_year].join(";")
     end
 
     def save_response(answer)
@@ -28,8 +33,9 @@ class FirstDataTransaction < Transaction
       self.success = answer.success?
       if answer.params
         self.response_code = answer.params['bank_resp_code']
+        self.response_auth_code = answer.params['authorization_num']
+        self.response_transaction_id = answer.params['transaction_tag']
       end
-      self.response_transaction_id = answer.authorization
       self.response_result = answer.message
       save
       super(answer)
