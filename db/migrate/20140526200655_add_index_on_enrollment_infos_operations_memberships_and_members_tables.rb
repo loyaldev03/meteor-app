36911e5bb9f6f08ee18cfbb7f014cf395d7dde07
Martin Ferrari <martin@xagax.com>
2014-05-26 17:12:44 -0300

    Add Indexes to Tables
    - enrollment_infos
    - operations
    - memberships
    - members

---
+++ db/migrate/20140526200655_add_index_on_enrollment_infos_operations_memberships_and_members_tables.rb
@@ -0,0 +1,15 @@
+class AddIndexOnEnrollmentInfosOperationsMembershipsAndMembersTables < ActiveRecord::Migration
+  def up
+  	execute "ALTER TABLE `enrollment_infos` ADD INDEX `index_created_at` (`created_at` DESC);"
+    execute "ALTER TABLE `operations` ADD INDEX `index_created_at` (`created_at` DESC);"
+    execute "ALTER TABLE `memberships` ADD INDEX `index_created_at` (`created_at` DESC);"
+    execute "ALTER TABLE `members` ADD INDEX `index_created_at` (`created_at` DESC);"
+  end
+
+  def down
+	execute "DROP INDEX index_created_at ON enrollment_infos"
+  	execute "DROP INDEX index_created_at ON operations"
+  	execute "DROP INDEX index_created_at ON memberships"
+  	execute "DROP INDEX index_created_at ON members"
+  end
+end
