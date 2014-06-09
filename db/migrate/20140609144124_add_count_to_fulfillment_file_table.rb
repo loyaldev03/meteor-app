3fd205bfce239fba91baa14d10aa921d61af69bc
Martin Ferrari <martin@xagax.com>
2014-06-09 15:45:22 -0300

    Column name replaced

--- db/migrate/20140609144124_add_count_to_fulfillment_file_table.rb
+++ db/migrate/20140609144124_add_count_to_fulfillment_file_table.rb
@@ -1,9 +1,9 @@
 class AddCountToFulfillmentFileTable < ActiveRecord::Migration
   def up
-  	add_column :fulfillment_files, :total, :integer, :default => 0
+  	add_column :fulfillment_files, :fulfillment_count, :integer, :default => 0
   end
 
   def down
-  	remove_column :fulfillment_files, :total
+  	remove_column :fulfillment_files, :fulfillment_count
   end
 end
