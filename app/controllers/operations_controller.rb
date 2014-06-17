bfc85c239e69db697864cac86ab337d7be2da33e
Martin Ferrari <martin@xagax.com>
2014-06-17 18:40:24 -0300

    [#72997538] Using model

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -16,15 +16,13 @@ class OperationsController < ApplicationController
   # GET /operations/1
   def show
     @operation = Operation.find(params[:id])
-    @member = Member.find(@operation.member_id)
-    my_authorize! :edit, Operation, @member.club_id
+    my_authorize! :edit, Operation, @operation.member.club_id
   end
 
   # PUT /operations/1
   def update
     operation = Operation.find(params[:id])
-    @member = Member.find(operation.member_id)
-    my_authorize! :edit, Operation, @member.club_id
+    my_authorize! :edit, Operation, operation.member.club_id
     @link = (view_context.link_to "#{operation.id}", operation_path(@current_partner.prefix,@current_club.name,@current_member.id,operation.id))
     if operation.update_attributes(params[:operation])
       message = "Edited operation note #{@link}".html_safe
