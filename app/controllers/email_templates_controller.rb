ed8fc4380170635d640c468bcc9f751f905dcb55
Martin Ferrari <martin@xagax.com>
2014-06-26 19:21:14 -0300

    [#73349630] Methods fixed
    Use nil for days after join date when type is pillar.
    Load tom ids

--- app/controllers/email_templates_controller.rb
+++ app/controllers/email_templates_controller.rb
@@ -15,12 +15,14 @@ class EmailTemplatesController < ApplicationController
 	def new
     my_authorize! :new, EmailTemplate, @current_club.id
 		@et = EmailTemplate.new
+		@tom = TermsOfMembership.find(params[:terms_of_membership_id])
+		@et.terms_of_membership_id = @tom.id
 	end
 
   def create
   	my_authorize! :create, EmailTemplate, @current_club.id
     @et = EmailTemplate.new(params[:email_template])
-    @tom = TermsOfMembership.find(params[:terms_of_membership_id])    
+    @tom = TermsOfMembership.find(params[:terms_of_membership_id])
     prepare_et_data_to_save(params)
     if @et.save
       redirect_to terms_of_membership_email_templates_url, :notice => "Your Communication #{@et.name} (ID: #{@et.id}) was successfully created"
@@ -37,17 +39,17 @@ class EmailTemplatesController < ApplicationController
 	end
 
 	def update
-		@et = EmailTemplate.find(params[:id])
-		@tom = TermsOfMembership.find(@et.terms_of_membership_id)
-    my_authorize! :update, EmailTemplate, @tom.club_id
-    prepare_et_data_to_save(params)
-    if @et.save
-      flash[:notice] = "Your Communication #{@et.name} (ID: #{@et.id}) was succesfully updated"
-      redirect_to terms_of_membership_email_templates_url
-    else
-      flash.now[:error] = "Your Communication was not updated."
-      render action: "edit"
-    end
+	@et = EmailTemplate.find(params[:id])
+	@tom = TermsOfMembership.find(@et.terms_of_membership_id)
+  my_authorize! :update, EmailTemplate, @tom.club_id
+  prepare_et_data_to_save(params)
+  if @et.save
+    flash[:notice] = "Your Communication #{@et.name} (ID: #{@et.id}) was succesfully updated"
+    redirect_to terms_of_membership_email_templates_url
+  else
+    flash.now[:error] = "Your Communication was not updated."
+    render action: "edit"
+  end
 	end
 
 	def show
@@ -72,8 +74,12 @@ class EmailTemplatesController < ApplicationController
 	def prepare_et_data_to_save(post_data)
 		@et.terms_of_membership_id = @tom.id
 		@et.template_type = post_data[:template_type]
-		@et.client = post_data[:client]
-		@et.days_after_join_date = post_data[:email_template][:days_after_join_date].to_i + 0
+		if @et.template_type == 'pillar'
+			@et.days_after_join_date = post_data[:email_template][:days_after_join_date].to_i
+		else
+			@et.days_after_join_date = nil
+		end
+		@et.client = post_data[:client]		
 		@et.external_attributes = post_data[:email_template][:external_attributes].sub(/\s+\Z/, '') == '' ? nil : post_data[:email_template][:external_attributes].sub(/\s+\Z/, '')
 	end
 
