dd538ffd9b34638d71b49ef4add134b743c8453c
Martin Ferrari <martin@xagax.com>
2014-06-26 19:22:47 -0300

    [[#73349630] Fixed method to load types in select
    Do not add type if it is currently used.

--- app/helpers/email_templates_helper.rb
+++ app/helpers/email_templates_helper.rb
@@ -1,17 +1,28 @@
 module EmailTemplatesHelper
-	def template_types_options
-		[
-			['Birthday', 'birthday'],
-			['Cancellation', 'cancellation'],
-			# ['Active', 'active'],
-			['Hard Decline', 'hard_decline'],
-			['Manual Payment Prebill', 'manual_payment_prebill'],
-			['Pillar', 'pillar'],
-			['Prebill', 'prebill'],
-			['Refund', 'refund'],
-			['Rejection', 'rejection'],
-			['Soft Decline', 'soft_decline']
+	def template_types_options(tom_id, current_type)
+		templates = [
+			"birthday", 
+			"cancellation", 
+			"hard_decline", 
+			"manual_payment_prebill", 
+			"pillar", 
+			"prebill", 
+			"refund", 
+			"rejection", 
+			"soft_decline"
 		]
+		templates_used_query = EmailTemplate.find_by_sql(["SELECT DISTINCT(template_type) FROM email_templates WHERE terms_of_membership_id = #{tom_id}"])
+		templates_used = Array.new()
+		templates_used_query.each do |tu|
+			templates_used = templates_used + [tu.template_type]
+		end
+		result = Array.new()
+		free_templates = templates - templates_used + Array(current_type) + ['pillar']
+		free_templates.each do |ft|
+			result << [ft.humanize, ft]
+		end
+		result.sort!
+		result
 	end
 
 	def clients_options
