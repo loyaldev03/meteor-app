aada93ae1b21dff57582880d7dfde1b9a49199d5
Martin Ferrari <martin@xagax.com>
2014-06-27 19:08:59 -0300

    [#73349630] Updated prepare method
    Added methods to retrieve data

--- app/controllers/email_templates_controller.rb
+++ app/controllers/email_templates_controller.rb
@@ -7,49 +7,49 @@ class EmailTemplatesController < ApplicationController
 		my_authorize! :create, EmailTemplate, @current_club.id
 		@tom = TermsOfMembership.find(params[:terms_of_membership_id])
 		respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: EmailTemplatesDatatable.new(view_context, @current_partner, @current_club, nil, @current_agent) }
-    end
+			format.html # index.html.erb
+			format.json { render json: EmailTemplatesDatatable.new(view_context, @current_partner, @current_club, nil, @current_agent) }
+		end
 	end
 
 	def new
-    my_authorize! :new, EmailTemplate, @current_club.id
+		my_authorize! :new, EmailTemplate, @current_club.id
 		@et = EmailTemplate.new
 		@tom = TermsOfMembership.find(params[:terms_of_membership_id])
 		@et.terms_of_membership_id = @tom.id
 	end
 
-  def create
-  	my_authorize! :create, EmailTemplate, @current_club.id
-    @et = EmailTemplate.new(params[:email_template])
-    @tom = TermsOfMembership.find(params[:terms_of_membership_id])
-    prepare_et_data_to_save(params)
-    if @et.save
-      redirect_to terms_of_membership_email_templates_url, :notice => "Your Communication #{@et.name} (ID: #{@et.id}) was successfully created"
-    else
-      flash.now[:error] = "There was an error while trying to save this Communication."
-      render action: "new"
-    end
-  end
+	def create
+		my_authorize! :create, EmailTemplate, @current_club.id
+		@et = EmailTemplate.new(params[:tom])
+		@tom = TermsOfMembership.find(params[:terms_of_membership_id]) 
+		prepare_et_data_to_save(params)
+		if @et.save!
+			redirect_to terms_of_membership_email_templates_url, :notice => "Your Communication #{@et.name} (ID: #{@et.id}) was successfully created"
+		else
+			flash.now[:error] = "There was an error while trying to save this Communication."
+			render action: "new"
+		end
+	end
 
 	def edit  
 		@et = EmailTemplate.find(params[:id])
 		@tom = TermsOfMembership.find(@et.terms_of_membership_id)
-    my_authorize! :edit, EmailTemplate, @tom.club_id
+		my_authorize! :edit, EmailTemplate, @tom.club_id
 	end
 
 	def update
-	@et = EmailTemplate.find(params[:id])
-	@tom = TermsOfMembership.find(@et.terms_of_membership_id)
-  my_authorize! :update, EmailTemplate, @tom.club_id
-  prepare_et_data_to_save(params)
-  if @et.save
-    flash[:notice] = "Your Communication #{@et.name} (ID: #{@et.id}) was succesfully updated"
-    redirect_to terms_of_membership_email_templates_url
-  else
-    flash.now[:error] = "Your Communication was not updated."
-    render action: "edit"
-  end
+		@et = EmailTemplate.find(params[:id])
+		@tom = TermsOfMembership.find(@et.terms_of_membership_id)
+		my_authorize! :update, EmailTemplate, @tom.club_id
+		prepare_et_data_to_save(params)
+		if @et.save!
+			flash[:notice] = "Your Communication #{@et.name} (ID: #{@et.id}) was succesfully updated"
+			redirect_to terms_of_membership_email_templates_url
+		else
+			flash.now[:error] = "Your Communication was not updated."
+			render action: "edit"
+		end
 	end
 
 	def show
@@ -61,17 +61,43 @@ class EmailTemplatesController < ApplicationController
 	def destroy
 		@et = EmailTemplate.find(params[:id])
 		@tom = TermsOfMembership.find(@et.terms_of_membership_id)
-    my_authorize! :destroy, EmailTemplate, @tom.club_id
-    if @et.destroy
-      flash[:notice] = "Communication #{@et.name} (ID: #{@et.id}) was successfully destroyed."
-    else
-      flash[:error] = "Communication #{@et.name} (ID: #{@et.id}) was not destroyed."
-    end
-    redirect_to terms_of_membership_email_templates_url
+		my_authorize! :destroy, EmailTemplate, @tom.club_id
+		if @et.destroy
+			flash[:notice] = "Communication #{@et.name} (ID: #{@et.id}) was successfully destroyed."
+		else
+			flash[:error] = "Communication #{@et.name} (ID: #{@et.id}) was not destroyed."
+		end
+		redirect_to terms_of_membership_email_templates_url
+	end
+
+	def external_attributes_html()
+		render :partial => "external_attributes_html.html.erb", :locals => { :client => params[:client], :ea => params[:ea] }
+	end
+
+	def clients_options
+	[
+		['Action Mailer', 'action_mailer'],
+		['Exact Target', 'exact_target'],
+		['Lyris ', 'lyris']
+	]
+	end
+
+	def external_attributes(client)
+		case client
+			when "action_mailer"
+				['trigger_id', 'mlid', 'site_id']
+			when 'exact_target'
+				['trigger_id', 'mlid', 'site_id', 'customer_key']
+			when 'lyris'
+				['trigger_id', 'mlid', 'site_id']
+			else
+				[]
+  	end
 	end
 
 	private
 	def prepare_et_data_to_save(post_data)
+		@et.name = post_data[:email_template][:name]
 		@et.terms_of_membership_id = @tom.id
 		@et.template_type = post_data[:template_type]
 		if @et.template_type == 'pillar'
@@ -79,8 +105,12 @@ class EmailTemplatesController < ApplicationController
 		else
 			@et.days_after_join_date = nil
 		end
-		@et.client = post_data[:client]		
-		@et.external_attributes = post_data[:email_template][:external_attributes].sub(/\s+\Z/, '') == '' ? nil : post_data[:email_template][:external_attributes].sub(/\s+\Z/, '')
+		@et.client = post_data[:client]
+		attributes = Hash.new()
+		ea_keys = external_attributes(@et.client)
+		ea_keys.each do |attrib|
+			attributes[attrib.to_sym] = post_data[attrib]
+		end
+		@et.external_attributes = attributes
 	end
-
 end
