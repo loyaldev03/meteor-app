4bfa68739faf3c960e88e23d179011148bb26175
sebastian <sebastian@sebastian-xagax.(none)>
2014-06-30 15:15:18 -0300

    [#3349630] improved code to fetch email templates that are available when creating/upating.

--- app/helpers/email_templates_helper.rb
+++ app/helpers/email_templates_helper.rb
@@ -33,20 +33,11 @@ module EmailTemplatesHelper
 			"rejection", 
 			"soft_decline"
 		]
-		templates_used_query = EmailTemplate.find_by_sql(["SELECT DISTINCT(template_type) FROM email_templates WHERE terms_of_membership_id = #{tom_id}"])
-		templates_used = Array.new()
-		templates_used_query.each do |tu|
-			templates_used = templates_used + [tu.template_type]
-		end
-		result = Array.new()
+		templates_used = TermsOfMembership.find(tom_id).email_templates.collect(&:template_type)
+		result = []
 		free_templates = templates - templates_used + Array(current_type)
-		if !free_templates.include?('pillar')
-			free_templates = free_templates + ['pillar']
-		end
-		free_templates.each do |ft|
-			result << [ft.humanize, ft]
-		end
+		free_templates = free_templates + ['pillar'] if !free_templates.include?('pillar')
+		free_templates.each{ |ft| result << [ft.humanize, ft] }
 		result.sort!
-		result
 	end
 end
