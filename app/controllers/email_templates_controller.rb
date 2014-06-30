5b410479ab34d691a0012a41050bc4e68f08de5a
Martin Ferrari <martin@xagax.com>
2014-06-30 17:53:37 -0300

    [#73349630] Fixed ET externa attribs

--- app/controllers/email_templates_controller.rb
+++ app/controllers/email_templates_controller.rb
@@ -121,7 +121,7 @@ class EmailTemplatesController < ApplicationController
 			when "action_mailer"
 				[]
 			when 'exact_target'
-				['trigger_id', 'mlid', 'site_id', 'customer_key']
+				['customer_key']
 			when 'lyris'
 				['trigger_id', 'mlid', 'site_id']
 			else
