5446f4664834758c820c5de150f11bdfe6d39fc5
Martin Ferrari <martin@xagax.com>
2014-06-30 13:28:38 -0300

    [#73349630] Realocated JS code

--- app/assets/javascripts/email_templates.js
+++
@@ -1,9 +0,0 @@
-  function switch_days_after_join_date() {
-    if ($("#template_type").val() == 'pillar') {
-      $("#control_group_days_after_join_date").show(100);
-    }
-    else {
-     $("#control_group_days_after_join_date").hide(100); 
-     $("#email_template_days_after_join_date").val(1);
-    }
-  }
