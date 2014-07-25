31af924fadd980a46ca6ef20b606bb1f280b0e57
Carla Ares <carla.ares@honou.com.ar>
2014-07-25 20:47:44 -0300

    setting class name acording to file name

--- test/integration/payment_gateway_configurations_test.rb
+++ test/integration/payment_gateway_configurations_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
-class TermsOfMembershipTests < ActionController::IntegrationTest
-	setup do
-		@admin_agent = FactoryGirl.create(:confirmed_admin_agent)
-		@partner = FactoryGirl.create(:partner)
-		@club = FactoryGirl.create(:club, :partner_id => @partner.id)
-		sign_in_as(@admin_agent)
-	end
+class PaymentGatewayConfigurationTest < ActionController::IntegrationTest
+  setup do
+    @admin_agent = FactoryGirl.create(:confirmed_admin_agent)
+    @partner = FactoryGirl.create(:partner)
+    @club = FactoryGirl.create(:club, :partner_id => @partner.id)
+    sign_in_as(@admin_agent)
+  end
 
   def fill_in_payment_gateway_configuration(new_pgc, new=true)
     select(I18n.t("activerecord.gateway.#{new_pgc.gateway}"), :from => 'payment_gateway_configuration[gateway]') if new
@@ -21,10 +21,10 @@ class TermsOfMembershipTests < ActionController::IntegrationTest
     confirm_ok_js
   end
 
-	test "Add PGC, Show a PGC and Edit PGC- Login by General Admin" do
+  test "Add PGC, Show a PGC and Edit PGC- Login by General Admin" do
     visit club_path(@partner.prefix, @club.id)
     click_link_or_button "New Payment Gateway"
-		new_pgc = FactoryGirl.build(:payment_gateway_configuration)
+    new_pgc = FactoryGirl.build(:payment_gateway_configuration)
     
     fill_in_payment_gateway_configuration(new_pgc)
     click_link_or_button "Create Payment gateway configuration"
@@ -49,7 +49,7 @@ class TermsOfMembershipTests < ActionController::IntegrationTest
     assert page.has_content? new_pgc.gateway
     assert page.has_content? "merchantKey"
     assert page.has_content? "newLogin"
-	end
+  end
 
   test "Add PGC and Show a PGC - Login by Admin by role" do
     @admin_agent.update_attribute :roles, nil
@@ -76,26 +76,26 @@ class TermsOfMembershipTests < ActionController::IntegrationTest
   end
 
 
-	test "Do not allow enter PGC duplicated - Login by General Admin" do
-		@club = FactoryGirl.create(:simple_club_with_gateway, :partner_id => @partner.id)
+  test "Do not allow enter PGC duplicated - Login by General Admin" do
+    @club = FactoryGirl.create(:simple_club_with_gateway, :partner_id => @partner.id)
     visit club_path(@partner.prefix, @club.id)
     click_link_or_button "Payment Gateway Configuration"
-		new_pgc = FactoryGirl.build(:payment_gateway_configuration)
+    new_pgc = FactoryGirl.build(:payment_gateway_configuration)
     click_link_or_button "Replace Payment Gateway"
 
     fill_in_payment_gateway_configuration(new_pgc)
     confirm_ok_js
     click_link_or_button "Create Payment gateway configuration"
     assert page.has_content? "Gateway already created. There is a payment gateway already configured for this gateway"
-	end
+  end
 
-	test "Do not allow to edit a PGC if it has members - Login by General Admin" do
-		@club = FactoryGirl.create(:simple_club_with_gateway, :partner_id => @partner.id)
+  test "Do not allow to edit a PGC if it has members - Login by General Admin" do
+    @club = FactoryGirl.create(:simple_club_with_gateway, :partner_id => @partner.id)
     @member = FactoryGirl.create(:member, :club_id => @club.id)
     visit club_path(@partner.prefix, @club.id)
 
     click_link_or_button "Payment Gateway Configuration"
     wait_until{ page.has_content? "Payment gateway configuration" }
     assert page.has_no_selector? "#new_payment_gateway"
-	end
+  end
 end
