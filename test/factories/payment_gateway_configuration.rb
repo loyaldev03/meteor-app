1e24d8f7628248bee59d9ed697c60515df9b7d7e
sebastian <sebastian@sebastian-xagax.(none)>
2014-08-22 14:20:06 -0300

    [#75895054] fixed pgc factories

--- test/factories/payment_gateway_configuration.rb
+++ test/factories/payment_gateway_configuration.rb
@@ -18,7 +18,6 @@ FactoryGirl.define do
 
   factory :authorize_net_payment_gateway_configuration, class: PaymentGatewayConfiguration do
     login  "7g6zBUa54"
-    merchant_key "SAC, Inc"
     password "7RkC6yG74etY545X"
     gateway "authorize_net"
     report_group "SAC_STAGING_TEST"
@@ -26,7 +25,6 @@ FactoryGirl.define do
 
   factory :first_data_payment_gateway_configuration, class: PaymentGatewayConfiguration do
     login  "AE6691-05"
-    merchant_key "SAC, Inc"
     password "fJmFbJ3AA"
     gateway "first_data"
     report_group "SAC_STAGING_TEST"
