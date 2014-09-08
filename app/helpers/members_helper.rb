dc30511e0ae06dbfd87e98ecef1541f0348ecd3a
sebastian <sebastian@sebastian-xagax.(none)>
2014-09-08 15:17:14 -0300

    removing unecessary change

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,30 +1,30 @@
-module MembersHelper
-  def member_action_drop_menu(current_member)
+module UsersHelper
+  def user_action_drop_menu(current_user)
     js_confirm = "return confirm(\"Continue?\");"
     html = ""
     html << "<div class='btn-group'>"
     html << "<a href='#' data-toggle='dropdown' class='btn dropdown-toggle'>Actions<span class='caret'></span></a>"
     html << "<ul class='dropdown-menu'>"
-    unless current_member.blacklisted?
-      html << "<li><a onclick='#{js_confirm}' href='#{member_blacklist_path(:member_prefix => current_member.id)}'>#{t('buttons.blacklist')}</a></li>"
+    unless current_user.blacklisted?
+      html << "<li><a onclick='#{js_confirm}' href='#{user_blacklist_path(:user_prefix => current_user.id)}'>#{t('buttons.blacklist')}</a></li>"
     end
-    if current_member.can_be_canceled?
-      html << "<li><a onclick='#{js_confirm}' href='#{member_cancel_path(:member_prefix => current_member.id)}'>#{t('buttons.cancel')}</a></li>"
+    if current_user.can_be_canceled?
+      html << "<li><a onclick='#{js_confirm}' href='#{user_cancel_path(:user_prefix => current_user.id)}'>#{t('buttons.cancel')}</a></li>"
     end
-    unless current_member.api_id.nil? 
-      html << "<li><a onclick='#{js_confirm}' href= #{member_resend_welcome_path(:member_prefix => current_member.id)} data-method='post' >#{t('buttons.resend_welcome_email')}</a></li>"
-      html << "<li><a onclick='#{js_confirm}' href= #{member_reset_password_path(:member_prefix => current_member.id)} data-method='post'>#{t('buttons.password_reset')}</a></li>"
+    unless current_user.api_id.nil? 
+      html << "<li><a onclick='#{js_confirm}' href= #{user_resend_welcome_path(:user_prefix => current_user.id)} data-method='post' >#{t('buttons.resend_welcome_email')}</a></li>"
+      html << "<li><a onclick='#{js_confirm}' href= #{user_reset_password_path(:user_prefix => current_user.id)} data-method='post'>#{t('buttons.password_reset')}</a></li>"
     end
     html << "</ul>"
     html << "</div>"
     raw(html)
   end
 
-  def menber_status_class(current_member)
+  def user_status_class(current_user)
     text = ""
-    if current_member.lapsed?
+    if current_user.lapsed?
       text = "btn-danger"
-    elsif current_member.active?
+    elsif current_user.active?
       text = "btn-success"
     else
       text = "btn-warning"
