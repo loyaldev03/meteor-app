a6b56585f52c0960bd5bb08eab47e87083b54010
sebastian <sebastian@sebastian-xagax.(none)>
2014-10-29 15:27:07 -0300

    added first test to check if jenkins is working properly with elasticsearch

--- test/integration/users_search_test.rb
+++ test/integration/users_search_test.rb
@@ -16,6 +16,7 @@ class UsersSearchTest < ActionController::IntegrationTest
   end
 
   def setup_user(create_new_user = true)
+    User.index.delete
     @default_state = "Alabama" # when we select options we do it by option text not by value ?
     @admin_agent = FactoryGirl.create(:confirmed_admin_agent)
     @partner = FactoryGirl.create(:partner)
@@ -35,11 +36,15 @@ class UsersSearchTest < ActionController::IntegrationTest
   #Only for search test
   def setup_search(create_new_users = true)
     setup_user false
+    Delayed::Worker.delay_jobs = true
     if create_new_users
       20.times{ create_active_user(@terms_of_membership_with_gateway, :active_user, nil, {}, { :created_by => @admin_agent }) }
       30.times{ create_active_user(@terms_of_membership_with_gateway, :lapsed_user, nil, {}, { :created_by => @admin_agent }) }
       30.times{ create_active_user(@terms_of_membership_with_gateway, :provisional_user, nil, {}, { :created_by => @admin_agent }) }
     end
+    Delayed::Job.all.each{ |x| x.invoke_job }
+    Delayed::Worker.delay_jobs = false
+
     @search_user = User.first
     visit users_path(:partner_prefix => @partner.prefix, :club_prefix => @club.name)
   end
@@ -49,8 +54,25 @@ class UsersSearchTest < ActionController::IntegrationTest
   ##########################################################
 
   test "search user by user id" do
-    setup_search
     search_user("user[id]", "#{@search_user.id}", @search_user)
   end
+
+  test "search user by first_name" do
+    setup_search
+    search_user("user[first_name]", "#{@search_user.first_name}", @search_user)
+  end
+
+  test "search user by last_name" do
+    setup_search
+    search_user("user[last_name]", "#{@search_user.last_name}", @search_user)
+  end
+
+  test "search user by email" do
+    setup_search
+    search_user("user[email]", "#{@search_user.email}", @search_user)
+    search_user("user[email]", "#{@search_user.email.split('@').first}*", @search_user)
+  end
+
+  
 
 end
