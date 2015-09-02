cd9d76f4bc759ad131fb1fecec24c02e60f489ec
Sebastian DL <delucasd@gmail.com>
2015-09-02 14:42:27 -0300

    [#101969104] set up devise to send emails asynchronously. Using devise-async gem

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,6 +8,7 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
+  config.mailer = "Devise::Async::Proxy"
 
   # Automatically apply schema changes in tableless databases
   config.apply_schema = false
