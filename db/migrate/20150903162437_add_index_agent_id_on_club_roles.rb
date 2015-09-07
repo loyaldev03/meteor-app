9fea68f18c4165990d42c40ddf16d597646bf8a0
Sebastian DL <delucasd@gmail.com>
2015-09-07 14:59:51 -0300

    [#101969104] configured so we cache some information from the header and also added a migration to add an index on agent_id column within club_roles

---
+++ db/migrate/20150903162437_add_index_agent_id_on_club_roles.rb
@@ -0,0 +1,9 @@
+class AddIndexAgentIdOnClubRoles < ActiveRecord::Migration
+  def up
+    execute "ALTER TABLE `club_roles` ADD INDEX `agent_id` (`agent_id` ASC);"
+  end
+
+  def down
+    execute "DROP INDEX agent_id ON enrollment_infos"
+  end
+end
