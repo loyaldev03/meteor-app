bbe42f14ddb77221a8129825864df11d31e3b65f
Sebastian DL <delucasd@gmail.com>
2015-11-13 16:10:01 -0300

    [#106640212] fixed bulk update to also accept mime type application/vnd.ms-excel for csv files uploaded from windows.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -63,7 +63,7 @@ class ProductsController < ApplicationController
 
   def bulk_update
     if params[:file]
-      if params[:file].content_type.include? 'text/csv'
+      if ['text/csv','application/vnd.ms-excel'].include? params[:file].content_type
         temporary_file = File.open("tmp/bulk_update_#{Time.current}.csv", "w")
         temporary_file.write params[:file].open.read
         temporary_file.close
