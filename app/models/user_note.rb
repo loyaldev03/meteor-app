d85b268f8e68d5cc13329f412cbc5bd2f2ffc90f
Sebastian DL <delucasd@gmail.com>
2015-12-16 15:40:44 -0300

    Fixed relationship between user_notes and agents to also include those deleted

--- app/models/user_note.rb
+++ app/models/user_note.rb
@@ -1,6 +1,6 @@
 class UserNote < ActiveRecord::Base
   attr_accessible :note_type, :description
-  belongs_to :created_by, class_name: 'Agent', foreign_key: 'created_by_id'# , with_deleted: true #FIXME paranoid
+  belongs_to :created_by, -> { with_deleted }, class_name: 'Agent', foreign_key: 'created_by_id'
   belongs_to :user
   belongs_to :disposition_type
   belongs_to :communication_type
