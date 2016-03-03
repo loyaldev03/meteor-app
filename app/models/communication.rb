a267da1b3e9f98037c1637675a293c55e60f275a
Sebastian DL <delucasd@gmail.com>
2016-03-03 16:33:03 -0300

    [#109173646] rolbacked change made by mistake within an error report in Communication model.

--- app/models/communication.rb
+++ app/models/communication.rb
@@ -58,7 +58,7 @@ class Communication < ActiveRecord::Base
     update_attributes sent_success: false, response: e, processed_at: Time.zone.now
     unless e.to_s.include?("Timeout")
       Auditory.report_issue("Communication deliver_exact_target", e, { user: user.id, 
-        current_membership: user.current_membership.id, communication: self.id })
+        current_membership: user.current_membership.id, communication: self.inspect })
       Auditory.audit(nil, self, "Error while sending communication '#{template_name}'.", user, Settings.operation_types["#{template_type}_email"])
     end
   end  
