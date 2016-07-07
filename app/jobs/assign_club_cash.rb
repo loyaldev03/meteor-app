8b60cdce13541c7e333025155a5080b08462955a
Martin Ferrari <dmferrari@gmail.com>
2016-07-07 16:53:26 -0300

    [#125138391] Using user method instead of sending it as parameter

--- app/jobs/assign_club_cash.rb
+++ app/jobs/assign_club_cash.rb
@@ -1,11 +1,11 @@
 class AssignClubCash < ActiveJob::Base
   queue_as :club_cash_queue
 
-  def perform(user_id, message, enroll = false, is_not_drupal = true)
+  def perform(user_id, message, enroll = false)
     user = User.find(user_id)
     amount = enroll ? user.terms_of_membership.initial_club_cash_amount : user.terms_of_membership.club_cash_installment_amount
     user.add_club_cash(nil, amount, message)
-    if is_not_drupal
+    if user.is_not_drupal?
       if user.club_cash_expire_date.nil? # first club cash assignment
         user.update_attribute :club_cash_expire_date, user.join_date + 1.year
       end
