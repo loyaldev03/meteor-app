38fbdc5957af04719a74f804b06efc228c4cfd51
Martin Ferrari <dmferrari@gmail.com>
2016-07-14 16:01:15 -0300

    [#118308773] Add edit button to datatable

--- app/datatables/campaign_datatable.rb
+++ app/datatables/campaign_datatable.rb
@@ -18,8 +18,8 @@ private
         campaign.transport,
         I18n.l(campaign.initial_date.to_date),
         I18n.l(campaign.finish_date.to_date),
-        (link_to(I18n.t(:show), @url_helpers.campaign_path(partner_prefix: @current_partner.prefix, club_prefix: @current_club.name, :id => campaign.id), :class => 'btn btn-mini') if @current_agent.can? :read, Campaign, @current_club.id).to_s
-
+        (link_to(I18n.t(:show), @url_helpers.campaign_path(partner_prefix: @current_partner.prefix, club_prefix: @current_club.name, :id => campaign.id), :class => 'btn btn-mini') if @current_agent.can? :read, Campaign, @current_club.id).to_s + 
+        (link_to(I18n.t(:edit), @url_helpers.edit_campaign_path(partner_prefix: @current_partner.prefix, club_prefix: @current_club.name, :id => campaign.id.to_s), :class => 'btn btn-mini') if @current_agent.can? :edit, Campaign, @current_club.id).to_s
       ]
     end
   end
