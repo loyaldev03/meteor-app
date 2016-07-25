8e91be68a5a5ac97152e59c505a540c5f709faae
Martin Ferrari <dmferrari@gmail.com>
2016-07-25 14:28:02 -0300

    [#126689783] Added controller views and datatables
    [#126689783] Ability for transport settings
    
    [#126689783] Added breadcrumb and menu item
    
    [#126689783] Added route for transport settings
    
    [#126689783] Custom update for model
    
    [#126689783] Updated controller methods
    
    [#126689783] 'New' template
    
    [#126689783] Functions for form
    
    [#126689783] Do not show them yet
    
    [#126689783] Updated form fields
    
    [#126689783] Updated strings
    
    [#126689783] Updated parameters

---
+++ app/controllers/transport_settings_controller.rb
@@ -0,0 +1,48 @@
+class TransportSettingsController < ApplicationController
+  before_action :set_transport, only: [:show, :edit, :update]
+
+  def index
+    # my_authorize! :list, TransportSetting, @current_club.id
+    respond_to do |format|
+      format.html
+      format.json { render json: TransportSetting.new(view_context, @current_partner, @current_club, @current_user, @current_agent)}
+    end 
+  end
+
+  def show
+  end
+
+  def new
+    @transport = TransportSetting.new
+  end
+
+  def edit
+  end
+
+  def create
+    @transport = TransportSetting.new(transport_params)
+
+    if @transport.save
+      redirect_to @transport, notice: 'Transport was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @transport.update(transport_params)
+      redirect_to @transport, notice: 'Transport was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_transport
+      @transport = TransportSetting.find(params[:id])
+    end
+
+    def transport_params
+      params.require(:transport_setting).permit(:transport, :fb_client_id, :fb_client_secret, :fb_access_token)
+    end
+end
--- app/models/transport_setting.rb
+++ app/models/transport_setting.rb
@@ -1,12 +1,19 @@
 class TransportSetting < ActiveRecord::Base
   belongs_to :club
 
+  before_update :custom_update
+
   enum transport: {
     facebook:   0,
     mailchimp:  1,
-    twitter:    2,
-    adwords:    3
+    # twitter:    2,
+    # adwords:    3
   }
 
   serialize :settings, JSON
+
+  private
+    def custom_update
+
+    end
 end
