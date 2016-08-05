14375b06b632c9c5136a2e0259110b93d2d9df8a
Sebastian De Luca <sebastiandeluca@Charlies-MacBook-Pro.local>
2016-08-05 10:33:55 -0300

    [#122069173] added some minnor fixes in campaign_days edit feature.

--- app/controllers/campaign_days_controller.rb
+++ app/controllers/campaign_days_controller.rb
@@ -2,7 +2,7 @@ class CampaignDaysController < ApplicationController
   before_filter :validate_club_presence
 
   def missing
-    my_authorize! :list, CampaignDay, current_club.id
+    my_authorize! :manage, CampaignDay, current_club.id
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: CampaignDaysDatatable.new(view_context, current_partner, current_club, nil, current_agent) }
@@ -10,18 +10,18 @@ class CampaignDaysController < ApplicationController
   end
 
   def edit
-    my_authorize! :edit, CampaignDay, current_club.id
+    my_authorize! :manage, CampaignDay, current_club.id
     @campaign_day = CampaignDay.find(params[:id])
     render partial: 'edit'
   end
 
   def update
     @campaign_day = CampaignDay.find(params[:id])
-    my_authorize! :update, CampaignDay, @campaign_day.campaign.club_id
+    my_authorize! :manage, CampaignDay, @campaign_day.campaign.club_id
     if @campaign_day.update_attributes params.require(:campaign_day).permit(:spent, :converted, :reached)
-      render json: { success: true, message: 'Campaign day #{@campaign_day.date} for Campaign #{@campaign_day.name} was update successfuly.' }
+      render json: { success: true, message: "Campaign day #{@campaign_day.date} for Campaign #{@campaign_day.campaign.name} was update successfuly." }
     else
-      render json: { success: false, message: 'Campaign day was not updated. Error: #{@campaign_day.errors.messages}' }
+      render json: { success: false, message: "Campaign day was not updated. Error: #{@campaign_day.errors.messages}" }
     end
   rescue ActiveRecord::RecordNotFound
     render json: { success: false, message: 'Campaign day not found.' }
