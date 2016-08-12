b755ffd15ec78514df31286c5dd01e9d6fb9c32d
Martin Ferrari <dmferrari@gmail.com>
2016-08-11 22:24:04 -0300

    [#126689783] Fixed datatable

--- app/datatables/transport_settings_datatable.rb
+++ app/datatables/transport_settings_datatable.rb
@@ -1,4 +1,5 @@
 class TransportSettingsDatatable < Datatable
+  include TransportSettingsHelper
 
 private
   def total_records
@@ -13,7 +14,8 @@ private
     transport_settings.map do |transport_setting|
       [ 
         transport_setting.id,
-        transport_setting.transport,
+        transport_setting.transport.humanize,
+        nice_settings(transport_setting.settings),
         (link_to(I18n.t(:show), @url_helpers.transport_setting_path(partner_prefix: @current_partner.prefix, club_prefix: @current_club.name, :id => transport_setting.id), :class => 'btn btn-mini') if @current_agent.can? :read, TransportSetting, @current_club.id).to_s + 
         (link_to(I18n.t(:edit), @url_helpers.edit_transport_setting_path(partner_prefix: @current_partner.prefix, club_prefix: @current_club.name, :id => transport_setting.id.to_s), :class => 'btn btn-mini') if @current_agent.can? :edit, TransportSetting, @current_club.id).to_s
       ]
@@ -26,13 +28,13 @@ private
 
   def fetch_transport_settings
     transport_settings = @current_club.transport_settings.order("#{sort_column} #{sort_direction}")
-    # if params[:sSearch].present?
-    #   transport_settings = transport_settings.where("transport like :search", search: "%#{params[:sSearch]}%")
-    # end
+    if params[:sSearch].present?
+      transport_settings = transport_settings.where("transport like :search", search: "%#{params[:sSearch]}%")
+    end
     transport_settings.page(page).per_page(per_page)
   end
 
   def sort_column
-    # TransportSetting.datatable_columns[params[:iSortCol_0].to_i]
+    TransportSetting.datatable_columns[params[:iSortCol_0].to_i]
   end
 end     
