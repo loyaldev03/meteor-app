c3e4c3a3cc98391c91bd5c120977d37c7febc8b4
Martin Ferrari <dmferrari@gmail.com>
2016-08-11 23:20:31 -0300

    [#126689783] Fixed strings

--- app/helpers/transport_settings_helper.rb
+++ app/helpers/transport_settings_helper.rb
@@ -2,7 +2,7 @@ module TransportSettingsHelper
   def nice_settings(settings)
     data = []
     settings.each do |key, value|
-      data << "#{key.humanize} = \"#{value}\""
+      data << "#{key}: <b>#{value}</b>"
     end
     data.join('<br />').html_safe
   end
