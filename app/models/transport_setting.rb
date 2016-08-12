03dbd4c381be9c32c03c1a556d5fea0ac44e410e
Martin Ferrari <dmferrari@gmail.com>
2016-08-11 22:22:20 -0300

    [#126689783] Added validations and load data from settings

--- app/models/transport_setting.rb
+++ app/models/transport_setting.rb
@@ -1,10 +1,14 @@
 class TransportSetting < ActiveRecord::Base
   belongs_to :club
 
+  before_validation :set_data
+
+  validates_presence_of :transport
+  validates :fb_client_id, :fb_client_secret, :fb_access_token, presence: true, if: "transport == 'facebook'"
+  validates :mc_api_key, presence: true, if: "transport == 'mailchimp'"
+
   attr_accessor :fb_client_id, :fb_client_secret, :fb_access_token
   attr_accessor :mc_api_key
-
-  before_validation :set_data
 
   enum transport: {
     facebook:   0,
@@ -15,15 +19,26 @@ class TransportSetting < ActiveRecord::Base
 
   serialize :settings, JSON
 
+  def self.datatable_columns
+    [ 'id', 'transport', 'settings' ]
+  end
+
+  def parse_settings!
+    case transport
+    when 'facebook'
+      self.fb_client_id = settings['client_id']
+      self.fb_client_secret = settings['client_secret']
+      self.fb_access_token = settings['access_token']
+    when 'mailchimp'
+      self.mc_api_key = settings['api_key']
+    end 
+  end
+
   private
     def set_data
       case transport
       when 'facebook'
-        self.settings = { 
-          client_id: fb_client_id, 
-          client_secret: fb_client_secret, 
-          access_token: fb_access_token 
-        }
+        self.settings = { client_id: fb_client_id, client_secret: fb_client_secret, access_token: fb_access_token }
       when 'mailchimp'
         self.settings = { api_key: mc_api_key }
       when 'twitter', 'adwords'
