4670f0c3f93ecf9a1879d034ded2d8ecc44b1cbf
Martin Ferrari <dmferrari@gmail.com>
2016-08-17 18:32:36 -0300

    [#126689783] Using store to save serialized data

--- app/models/transport_setting.rb
+++ app/models/transport_setting.rb
@@ -1,14 +1,15 @@
 class TransportSetting < ActiveRecord::Base
   belongs_to :club
 
-  before_validation :set_data
+  store :settings, accessors: [ 
+    :client_id, :client_secret, :access_token,
+    :api_key
+  ], coder: JSON
 
+  validates_presence_of :club
   validates_presence_of :transport
-  validates :fb_client_id, :fb_client_secret, :fb_access_token, presence: true, if: -> { facebook? }
-  validates :mc_api_key, presence: true, if: -> { mailchimp? }
-
-  attr_accessor :fb_client_id, :fb_client_secret, :fb_access_token
-  attr_accessor :mc_api_key
+  validates :client_id, :client_secret, :access_token, presence: true, if: -> { facebook? }
+  validates :api_key, presence: true, if: -> { mailchimp? }
 
   enum transport: {
     facebook:   0,
@@ -17,18 +18,11 @@ class TransportSetting < ActiveRecord::Base
     # adwords:    3
   }
 
-  serialize :settings, JSON
-
   def self.datatable_columns
     ['id', 'transport']
   end
 
   private
-    def set_data
-      self.settings = { client_id: fb_client_id, client_secret: fb_client_secret, access_token: fb_access_token } if facebook?
-      self.settings = { api_key: mc_api_key } if mailchimp?
-    end
-
     scope :by_transport, -> (transport) {
       unless transport.kind_of? Integer
         transport = Campaign.transports[transport.to_s]
