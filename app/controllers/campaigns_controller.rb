1813102c3716c4934a04c761bcfa59bbfd2f31b6
Martin Ferrari <dmferrari@gmail.com>
2016-08-23 15:43:25 -0300

    [#128288511] Back to edit if can not save

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -46,7 +46,8 @@ class CampaignsController < ApplicationController
     if @campaign.update campaign_params_on_update
       redirect_to campaigns_url, notice: "Campaign <b>#{@campaign.name}</b> was updated succesfully".html_safe
     else
-      redirect_to campaigns_url, error: "Campaign <b>#{@campaign.name}</b> was not updated".html_safe
+      flash[:error] = "Campaign <b>#{@campaign.name}</b> was not updated".html_safe
+      render action: "edit"
     end
   end
 
