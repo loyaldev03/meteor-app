4ec402a9c9731548d4e6878d1690c76b6146030f
oni <sonia@xagax.com>
2016-08-29 13:26:41 -0300

    [#148010219] Add Landing name to campaigns factory

--- test/factories/campaign.rb
+++ test/factories/campaign.rb
@@ -2,10 +2,11 @@ FactoryGirl.define do
 
   factory :campaign do
     sequence(:name) {|n| "Campaign_#{Faker::Commerce.product_name}" }
+    landing_name {Faker::Lorem.characters(10)}
     initial_date Time.zone.now
     finish_date Time.zone.now + 7.days    
     enrollment_price { Faker::Number.decimal(2) }
-    campaign_type 0
+    campaign_type { Campaign.campaign_types['sloop'] } 
     transport { Campaign.transports['facebook'] }    
     transport_campaign_id  {Faker::Lorem.characters(20)}   
     campaign_medium "display"
