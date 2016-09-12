46233701aafa7f875add296ab937512218d370b4
Sebastian De Luca <delucasd@gmail.com>
2016-09-12 16:53:37 -0300

    [#129133963] added some missing indexes based on active record doctor… (#630)
    * [#129133963] added some missing indexes based on active record doctor analysis
    
    * [#129133963] removed unwanted index from table user_additional_data.
    
    * [#129133963] removed unwanted index from table user_additional_data.

---
+++ db/migrate/20160909121536_add_missing_indexes_on_foraign_keys.rb
@@ -0,0 +1,16 @@
+class AddMissingIndexesOnForaignKeys < ActiveRecord::Migration
+  def change
+    add_index :club_roles, :club_id 
+    add_index :domains, :partner_id
+    add_index :enumerations, :club_id
+    add_index :fulfillment_files, :agent_id
+    add_index :fulfillment_files, :club_id
+    add_index :fulfillment_files_fulfillments, :fulfillment_id
+    add_index :memberships, :created_by_id
+    add_index :operations, :club_id
+    add_index :operations, :created_by_id
+    add_index :prospects, :club_id
+
+    remove_index :users, :club_id
+  end
+end
