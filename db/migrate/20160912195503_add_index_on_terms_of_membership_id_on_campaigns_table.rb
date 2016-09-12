4fc4c4e5022032558844fda2b89ced5a33bd9703
Sebastian De Luca <sebastiandeluca@Charlies-MacBook-Pro.local>
2016-09-12 18:08:32 -0300

    [#129133963] added index on campaign's table on column terms_of_membership_id.

--- db/migrate/20160912195503_add_index_on_terms_of_membership_id_on_campaigns_table.rb
+++ db/migrate/20160912195503_add_index_on_terms_of_membership_id_on_campaigns_table.rb
@@ -1,4 +1,4 @@
-class ModifyIndexesRealtedToCampaigns < ActiveRecord::Migration
+class AddIndexOnTermsOfMembershipIdOnCampaignsTable < ActiveRecord::Migration
   def change
     add_index :campaigns, :terms_of_membership_id
   end
