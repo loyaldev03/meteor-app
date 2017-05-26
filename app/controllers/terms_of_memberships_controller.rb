95929ad853920f7cc2078691be5dec847e8f7287
Martin Ferrari <dmferrari@gmail.com>
2017-05-26 16:45:34 -0300

    [#145305625] Hide certain price points for SPs to streamline the process of Saving the Sale
    [#145305625] Added JS logic

--- app/controllers/terms_of_memberships_controller.rb
+++ app/controllers/terms_of_memberships_controller.rb
@@ -1,12 +1,12 @@
 class TermsOfMembershipsController < ApplicationController
   before_filter :validate_club_presence
-  before_filter :validate_pgc_presence, only: [ :new, :create ]
+  before_filter :validate_pgc_presence, only: [:new, :create]
 
   def index
     my_authorize! :list, TermsOfMembership, @current_club.id
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: TermsOfMembershipDatatable.new(view_context,@current_partner,@current_club,nil,@current_agent) }
+      format.json { render json: TermsOfMembershipDatatable.new(view_context, @current_partner, @current_club, nil, @current_agent) }
     end
   end
 
@@ -20,20 +20,20 @@ class TermsOfMembershipsController < ApplicationController
     my_authorize! :create, TermsOfMembership, @current_club.id
     prepare_tom_data_to_save(params)
     if @tom.save
-      redirect_to terms_of_memberships_url, :notice => "Your Subscription Plan #{@tom.name} (ID: #{@tom.id}) was created succesfully"
+      redirect_to terms_of_memberships_url, notice: "Your Subscription Plan #{@tom.name} (ID: #{@tom.id}) was created succesfully"
     else
       flash[:error] = 'There was an error while trying to save this subscription plan.'
-      render action: "new"
+      render action: 'new'
     end
   end
 
   def edit
     @tom = TermsOfMembership.find(params[:id])
-    my_authorize! :edit, TermsOfMembership, @tom.club_id 
-    if !@tom.can_update?
+    my_authorize! :edit, TermsOfMembership, @tom.club_id
+    unless @tom.can_update?
       redirect_to terms_of_memberships_url, alert: "Subscription Plan #{@tom.name} (ID: #{@tom.id}) can not be edited. It is being used"
     end
-  rescue ActiveRecord::RecordNotFound 
+  rescue ActiveRecord::RecordNotFound
     redirect_to terms_of_memberships_url, alert: 'Subscription Plan not found.'
   end
 
@@ -46,7 +46,7 @@ class TermsOfMembershipsController < ApplicationController
         redirect_to terms_of_memberships_url, notice: "Your Subscription Plan #{@tom.name} (ID: #{@tom.id}) was updated succesfully"
       else
         flash[:error] = 'Your Subscription Plan was not updated.'
-        render action: "edit"
+        render action: 'edit'
       end
     else
       redirect_to terms_of_memberships_url, alert: "Subscription Plan #{@tom.name} (ID: #{@tom.id}) can not be edited. It is being used"
@@ -62,7 +62,7 @@ class TermsOfMembershipsController < ApplicationController
       flash[:error] = "Subscription Plan #{@tom.name} (ID: #{@tom.id}) was not destroyed: " + (@tom.errors.messages[:base].first ? @tom.errors.messages[:base].first : '')
     end
     redirect_to terms_of_memberships_url
-  rescue ActiveRecord::RecordNotFound 
+  rescue ActiveRecord::RecordNotFound
     redirect_to terms_of_memberships_url, alert: 'Subscription Plan not found.'
   end
 
@@ -71,74 +71,75 @@ class TermsOfMembershipsController < ApplicationController
     my_authorize! :show, TermsOfMembership, @tom.club_id
     @email_templates = @tom.email_templates.where(client: @tom.club.marketing_tool_client)
     @payment_gateway_configuration = @tom.club.payment_gateway_configuration
-  rescue ActiveRecord::RecordNotFound 
+  rescue ActiveRecord::RecordNotFound
     redirect_to terms_of_memberships_url, alert: 'Subscription Plan not found.'
   end
 
   def resumed_information
     @tom = TermsOfMembership.find(params[:terms_of_membership_id])
     my_authorize! :show, TermsOfMembership, @tom.club_id
-    render :partial => "resumed_information", :locals => { :tom => @tom }
+    render partial: 'resumed_information', locals: { tom: @tom }
   end
 
   private
-    def prepare_tom_data_to_save(post_data)
-      @tom.club_id = @current_club.id
-      @tom.agent_id = @current_agent.id
-      # Step #1
-      @tom.name = post_data[:terms_of_membership][:name]
-      @tom.api_role = post_data[:terms_of_membership][:api_role]
-      @tom.description = post_data[:terms_of_membership][:description]
-      # Step 2
-      @tom.needs_enrollment_approval = post_data[:terms_of_membership][:needs_enrollment_approval]
-      @tom.initial_fee = post_data[:initial_fee_amount]
-      @tom.trial_period_amount = post_data[:trial_period_amount]
-      @tom.provisional_days = post_data[:trial_period_lasting_time_span] == 'months' ? months_to_days(post_data[:trial_period_lasting].to_i) : post_data[:trial_period_lasting].to_i
-      @tom.club_cash_installment_amount = post_data[:terms_of_membership][:club_cash_installment_amount]
-      @tom.is_payment_expected = post_data[:is_payment_expected] == 'yes' ? true : false
-      if @tom.is_payment_expected
-        @tom.installment_amount = post_data[:installment_amount]
-        @tom.installment_period = post_data[:installment_amount_days_time_span] == 'months' ? months_to_days(post_data[:installment_amount_days].to_i) : post_data[:installment_amount_days].to_i
-      else
-        @tom.installment_period = nil
-        @tom.installment_amount = nil
-        @tom.club_cash_installment_amount = nil
-      end
-      @tom.subscription_limits = post_data[:subscription_terms] == 'until_cancelled' ? 0 : (post_data[:subscription_terms_stop_billing_after_time_span] == 'months' ? months_to_days(post_data[:subscription_terms_stop_billing_after].to_i) : post_data[:subscription_terms_stop_billing_after].to_i)
-      @tom.initial_club_cash_amount = post_data[:terms_of_membership][:initial_club_cash_amount]
-      @tom.skip_first_club_cash = post_data[:terms_of_membership][:skip_first_club_cash]
-      # Step 3
-      case post_data[:if_cannot_bill_user]
-      when 'cancel'
-        @tom.if_cannot_bill = 'cancel'
-        @tom.downgrade_tom_id = nil
-        @tom.suspension_period = nil
-      when 'suspend'
-        @tom.if_cannot_bill = 'suspend'
-        @tom.suspension_period = post_data[:if_cannot_bill_user_suspend_for_time_span] == 'months' ? months_to_days(post_data[:if_cannot_bill_user_suspend_for].to_i) : post_data[:if_cannot_bill_user_suspend_for].to_i
-        @tom.downgrade_tom_id = nil
-      when 'downgrade_to'
-        @tom.if_cannot_bill = 'downgrade_tom'
-        @tom.downgrade_tom_id = post_data[:downgrade_to_tom]
-        @tom.suspension_period = nil
-      end
-      if post_data[:upgrade_to_tom] != ''
-        @tom.upgrade_tom_id = post_data[:upgrade_to_tom]
-        @tom.upgrade_tom_period = post_data[:upgrade_to_tom_days_time_span] == 'months' ? months_to_days(post_data[:upgrade_to_tom_days].to_i) : post_data[:upgrade_to_tom_days].to_i
-      else 
-        @tom.upgrade_tom_id = nil
-        @tom.upgrade_tom_period = nil
-      end
-    end
 
-    def months_to_days(months)
-      months = months.to_i
-      months > 0 ? (months * 30.4166667).to_i : 0
+  def prepare_tom_data_to_save(post_data)
+    @tom.club_id = @current_club.id
+    @tom.agent_id = @current_agent.id
+    # Step #1
+    @tom.name = post_data[:terms_of_membership][:name]
+    @tom.api_role = post_data[:terms_of_membership][:api_role]
+    @tom.description = post_data[:terms_of_membership][:description]
+    @tom.show_in_save_the_sale = post_data[:terms_of_membership][:show_in_save_the_sale]
+    # Step 2
+    @tom.needs_enrollment_approval = post_data[:terms_of_membership][:needs_enrollment_approval]
+    @tom.initial_fee = post_data[:initial_fee_amount]
+    @tom.trial_period_amount = post_data[:trial_period_amount]
+    @tom.provisional_days = post_data[:trial_period_lasting_time_span] == 'months' ? months_to_days(post_data[:trial_period_lasting].to_i) : post_data[:trial_period_lasting].to_i
+    @tom.club_cash_installment_amount = post_data[:terms_of_membership][:club_cash_installment_amount]
+    @tom.is_payment_expected = post_data[:is_payment_expected] == 'yes' ? true : false
+    if @tom.is_payment_expected
+      @tom.installment_amount = post_data[:installment_amount]
+      @tom.installment_period = post_data[:installment_amount_days_time_span] == 'months' ? months_to_days(post_data[:installment_amount_days].to_i) : post_data[:installment_amount_days].to_i
+    else
+      @tom.installment_period = nil
+      @tom.installment_amount = nil
+      @tom.club_cash_installment_amount = nil
     end
+    @tom.subscription_limits = post_data[:subscription_terms] == 'until_cancelled' ? 0 : (post_data[:subscription_terms_stop_billing_after_time_span] == 'months' ? months_to_days(post_data[:subscription_terms_stop_billing_after].to_i) : post_data[:subscription_terms_stop_billing_after].to_i)
+    @tom.initial_club_cash_amount = post_data[:terms_of_membership][:initial_club_cash_amount]
+    @tom.skip_first_club_cash = post_data[:terms_of_membership][:skip_first_club_cash]
+    # Step 3
+    case post_data[:if_cannot_bill_user]
+    when 'cancel'
+      @tom.if_cannot_bill = 'cancel'
+      @tom.downgrade_tom_id = nil
+      @tom.suspension_period = nil
+    when 'suspend'
+      @tom.if_cannot_bill = 'suspend'
+      @tom.suspension_period = post_data[:if_cannot_bill_user_suspend_for_time_span] == 'months' ? months_to_days(post_data[:if_cannot_bill_user_suspend_for].to_i) : post_data[:if_cannot_bill_user_suspend_for].to_i
+      @tom.downgrade_tom_id = nil
+    when 'downgrade_to'
+      @tom.if_cannot_bill = 'downgrade_tom'
+      @tom.downgrade_tom_id = post_data[:downgrade_to_tom]
+      @tom.suspension_period = nil
+    end
+    if post_data[:upgrade_to_tom] != ''
+      @tom.upgrade_tom_id = post_data[:upgrade_to_tom]
+      @tom.upgrade_tom_period = post_data[:upgrade_to_tom_days_time_span] == 'months' ? months_to_days(post_data[:upgrade_to_tom_days].to_i) : post_data[:upgrade_to_tom_days].to_i
+    else
+      @tom.upgrade_tom_id = nil
+      @tom.upgrade_tom_period = nil
+    end
+  end
 
-    def validate_pgc_presence
-      unless current_club.payment_gateway_configurations.first.present?
-        redirect_to new_payment_gateway_configuration_path(club_prefix: current_club.name), alert: t('activerecord.attributes.terms_of_membership.no_pgc_configured') 
-      end
-    end
+  def months_to_days(months)
+    months = months.to_i
+    months > 0 ? (months * 30.4166667).to_i : 0
+  end
+
+  def validate_pgc_presence
+    return if current_club.payment_gateway_configurations.first.present?
+    redirect_to new_payment_gateway_configuration_path(club_prefix: current_club.name), alert: t('activerecord.attributes.terms_of_membership.no_pgc_configured')
+  end
 end
