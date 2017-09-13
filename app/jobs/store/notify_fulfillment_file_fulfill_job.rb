201cd4fc1e219d4ea8f57c8baba8c4cbc55ccb15
Sebastian <delucasd@gmail.com>
2017-09-13 15:30:24 -0300

    Fixed minnor bug within the notifyer related to fulfillment sent notification

--- app/jobs/store/notify_fulfillment_file_fulfill_job.rb
+++ app/jobs/store/notify_fulfillment_file_fulfill_job.rb
@@ -5,7 +5,7 @@ module Store
     def perform(fulfillment_file)
       fulfillments_with_errors = fulfillment_file.fulfillments.where.not(sync_result: 'success').select(:fulfillment_id, :user_id, :sync_result)
       success_count = fulfillment_file.fulfillments.where(sync_result: 'success').count
-      Auditory.management_stock_notification('Error while notifying fulfillment send to the Store', "There have been some errors while notifying some fulfillment's send in fulfillment file ID##{fulfillment_file.id}. Details are below:", fulfillments_with_errors) if fulfillments_with_errors.any?
+      Auditory.management_stock_notification('Error while notifying fulfillment send to the Store', "There have been some errors while notifying some fulfillment's send in fulfillment file ID##{fulfillment_file.id}. Details are below:", fulfillments_with_errors.map{|x| "Fulfillment ID##{x.fulfillment_id}: #{x.sync_result}"}) if fulfillments_with_errors.any?
       StoreNotifier.fulfillment_file_send_results(fulfillment_file.agent_id, fulfillment_file.id, success_count, fulfillments_with_errors.map{|x| x[:fulfillment_id]}).deliver_later!
     end
   end
