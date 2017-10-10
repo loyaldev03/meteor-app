b9209ac6d74f2c5cb8aa5c0aaa87afc6744a3709
Sebastian <delucasd@gmail.com>
2017-10-10 09:30:44 -0300

    [#150439893] removing phone fields update from migration since it takes too long to run in production env.

--- db/migrate/20171004143522_change_phone_column_type_of_users.rb
+++ db/migrate/20171004143522_change_phone_column_type_of_users.rb
@@ -5,13 +5,6 @@ class ChangePhoneColumnTypeOfUsers < ActiveRecord::Migration
       t.change :phone_area_code,    :string, limit: 10
       t.change :phone_local_number, :string, limit: 10
     end
-    User.find_each do |user|
-      user.update_columns(
-        phone_country_code: format('%03d', user.phone_country_code.to_i),
-        phone_area_code:    format('%03d', user.phone_area_code.to_i),
-        phone_local_number: format('%07d', user.phone_local_number.to_i)
-      )
-    end
   end
 
   def down
