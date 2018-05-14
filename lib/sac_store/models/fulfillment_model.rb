a74ef9a620b3b96c05833bfd9c5e6b05fd941a66
Sebastian De Luca <delucasd@gmail.com>
2018-05-14 09:02:25 -0300

    [#156950454] improved fulfillment assignation rescue to provide more … (#798)
    * [#156950454] improved fulfillment assignation rescue to provide more information about the error and also to notify PT instead of Rollbar.
    
    * Improved Fulfillment sync with store method to let us know when there is a problem syncing the fulfillment against the store
    
    * Improved Fulfillment library to properly notify errors when fulfillment is not synced against Store
    
    * Improved Fulfillment library to properly notify errors when fulfillment is not synced against Store
    
    * Improved Fulfillment library to properly notify errors when fulfillment is not synced against Store
    
    * Fixed syntax error within fulfillment library for sac_store
    
    * Fixed syntax error within fulfillment library for sac_store
    
    * Improved code related to fulfillment sync with Story

--- lib/sac_store/models/fulfillment_model.rb
+++ lib/sac_store/models/fulfillment_model.rb
@@ -5,12 +5,12 @@ module SacStore
       unless fulfillment.store_id
         response = conn.post FULFILLMENT_ASSIGNATION_URL, { api_key: settings['api_token'], variant_id: fulfillment.product.store_id.to_s }
         result   = response.body
-        fulfillment.sync_result = if result and result.success
+        fulfillment.sync_result = if is_request_successful(result)
           fulfillment.store_id = result.stock_movement_id
           Product.where(id: fulfillment.product_id).update_all "stock = #{result.current_stock}"
           'success'
         else
-          result.message
+          handle_error_on_call(fulfillment, result, 'Fulfillment::NotifyFulfillmentAssignation')
         end
         fulfillment.save(validate: false)
       end
@@ -23,12 +23,11 @@ module SacStore
       if fulfillment.store_id
         response = conn.post FULFILLMENT_CANCELLATION_URL, { api_key: settings['api_token'], stock_movement_id: fulfillment.store_id.to_s }
         result   = response.body
-        fulfillment.sync_result = if result and result.success
+        fulfillment.sync_result = if is_request_successful(result)
           fulfillment.store_id = nil
           'success'
         else
-          Auditory.report_issue("Fulfillment::NotifyFulfillmentCancelation", nil, { response: response.inspect, fulfillment_id: fulfillment.id, club: fulfillment.club_id })
-          result.message
+          handle_error_on_call(fulfillment, result, 'Fulfillment::NotifyFulfillmentCancelation')
         end
         fulfillment.save(validate: false)
       end
@@ -40,10 +39,10 @@ module SacStore
     def notify_fulfillment_send
       response = conn.post FULFILLMENT_SENT_URL, { api_key: settings['api_token'], stock_movement_id: fulfillment.store_id.to_s }
       result   = response.body
-      fulfillment.sync_result = if result and (result.success or result.code == 400)
+      fulfillment.sync_result = if is_request_successful(result) or (result and result.code == 400)
         'success'
       else
-        result.message
+        handle_error_on_call(fulfillment, result, 'Fulfillment::NotifyFulfillmentSend')
       end
       fulfillment.save(validate: false)
     rescue => e
@@ -52,6 +51,15 @@ module SacStore
     end
 
     private
+      def is_request_successful(result)
+        result and not result.is_a?(String) and result.success
+      end
+    
+      def handle_error_on_call(fulfillment, result, method)
+        message = result.is_a? String ? result : result.message
+        Auditory.notify_pivotal_tracker(message, method, {fulfillment_id: fulfillment.id, club: fulfillment.club_id})
+        message
+      end
 
       def settings
         @settings ||= self.fulfillment.club.transport_settings.store_spree.first.settings
