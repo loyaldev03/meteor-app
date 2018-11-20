23719c8f6bb26110df0faba547d3cc8a939c7677
Sebastian <delucasd@gmail.com>
2018-11-20 11:57:03 -0300

    minor changes in code

--- config/initializers/api_integration.rb
+++ config/initializers/api_integration.rb
@@ -3,8 +3,8 @@ require 'sac_spree/spree'
 Drupal.logger = Rails.logger
 Spree.logger  = Rails.logger
 # in test env, integration should be manually enabled in specific tests
-Drupal.enable_integration! #unless Rails.env.test? or Rails.env.development?
-Spree.enable_integration! #unless Rails.env.test? or Rails.env.development?
+Drupal.enable_integration! unless Rails.env.test? or Rails.env.development?
+Spree.enable_integration! unless Rails.env.test? or Rails.env.development?
 
 
 # require 'sac_wordpress/wordpress'
