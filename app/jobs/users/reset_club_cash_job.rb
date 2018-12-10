2aa9d125b8df98a1f03da0f10bb6b7ddac3c047d
Sebastian <delucasd@gmail.com>
2018-12-10 13:54:35 -0300

    fixed reset club cash job logic

--- app/jobs/users/reset_club_cash_job.rb
+++ app/jobs/users/reset_club_cash_job.rb
@@ -6,9 +6,10 @@ module Users
       user = User.find user_id
       if user.club.allow_club_cash_transaction?
         if user.is_spree? and !user.terms_of_membership.freemium?
-          amount_to_reset   = (user.terms_of_membership.initial_club_cash_amount + (Settings.vip_additional_club_cash if user.vip_member?).to_i) - user.club_cash_amount
+          amount_to_reset = user.terms_of_membership.initial_club_cash_amount + (Settings.vip_additional_club_cash if user.vip_member?).to_i
+          amount_to_add   = amount_to_reset - user.club_cash_amount
           if amount_to_reset > 0
-            user.add_club_cash(nil, amount_to_reset, "Reseting Club cash cash amount to #{amount_to_reset} for #{user.vip_member? ? 'vip member' : 'paid member'}.")
+            user.add_club_cash(nil, amount_to_add, "Reseting Club cash cash amount to #{amount_to_reset} for #{user.vip_member? ? 'vip member' : 'paid member'}.")
           end
         elsif !user.is_drupal?
           amount_to_reset   = -user.club_cash_amount
