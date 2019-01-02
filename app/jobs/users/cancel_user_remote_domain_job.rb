ce263036a7d3502fddcda7d7503ff57d374f74f9
Sebastian <delucasd@gmail.com>
2019-01-02 14:42:40 -0300

    Fixed cancellation remote domain job.

--- app/jobs/users/cancel_user_remote_domain_job.rb
+++ app/jobs/users/cancel_user_remote_domain_job.rb
@@ -3,11 +3,11 @@ module Users
     queue_as :drupal_queue
 
     def perform(user_id:)
-      user  = User.find user_id
-      if user.api_user and !user.api_id.nil? and user.club.billing_enable
-        res = user.api_user.destroy! 
-        raise 'CancelUserRemoteDomainJob::UnexpectedError: CMS Account not destroyed.' if res.nil?
-        raise res.body['error_message'] if !res.body['success']
+      user = User.find user_id
+
+      if user.api_user && !user.api_id.nil? && user.club.billing_enable
+        res = user.api_user.destroy!
+        raise 'CancelUserRemoteDomainJob::UnexpectedError: CMS Account not destroyed.' if res.nil? || !res.success?
       end
     end
   end
